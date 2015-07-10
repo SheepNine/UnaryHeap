b3486543f5beeb0297c59d32ec7d9b9e9b34aeec
SheepNine_cp <sheepnine@gmail.com>
2015-07-10 02:49:21 +0000

    Add unit tests to build script

---
+++ source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/LinearFeedbackShiftRegisterTests.cs
@@ -0,0 +1,15 @@
+﻿using UnaryHeap.Utilities;
+using Xunit;
+
+namespace UnaryHeap.Utilities.Tests
+{
+    public class LinearFeedbackShiftRegisterTests
+    {
+        [Fact]
+        public void Length31Cycle()
+        {
+            var sut = new LinearFeedbackShiftRegister(15, new[] { 0, 6 }) { RegisterValue = 0x737 };
+            Assert.Equal("1110110011100001101010010001011", sut.GetCyclePattern());
+        }
+    }
+}
