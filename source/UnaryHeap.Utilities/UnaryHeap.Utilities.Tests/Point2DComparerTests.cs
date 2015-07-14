8079dc7a9d88edc028681aac2a0ab67109887997
SheepNine_cp <sheepnine@gmail.com>
2015-07-14 04:03:53 +0000

    Add class to sort Point2D objects according to a desired ordering

---
+++ source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/Point2DComparerTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnaryHeap.Utilities.Tests
+{
+    public class Point2DComparerTests
+    {
+        [Theory]
+        [MemberData("SortOrders")]
+        public void TestComparison(bool sortFirstByX, bool sortXDescending, bool sortYDescending, Point2D[] expected)
+        {
+            var inputData = SortInputData;
+            Array.Sort(inputData, new Point2DComparer(sortFirstByX, sortXDescending, sortYDescending));
+            Assert.Equal(expected, inputData);
+        }
+
+        [Fact]
+        public void DefaultConfiguration()
+        {
+            var a = SortInputData;
+            var b = SortInputData;
+
+            Array.Sort(a, new Point2DComparer(false, false, false));
+            Array.Sort(b, new Point2DComparer());
+
+            Assert.Equal(a, b);
+        }
+
+        public static Point2D[] SortInputData
+        {
+            get
+            {
+                return new[] {
+                    new Point2D(-1, -1),
+                    new Point2D(01, 00),
+                    new Point2D(-1, 01), 
+                    null,
+                    new Point2D(01, 01),
+                    null,
+                    new Point2D(-1, 00),               
+                    new Point2D(01, -1),
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> SortOrders
+        {
+            get
+            {
+                return new[]{
+                    new object[] { false, false, false, new [] { null, null, new Point2D(-1, -1), new Point2D(01, -1), new Point2D(-1, 00), new Point2D(01, 00), new Point2D(-1, 01), new Point2D(01, 01) } },
+                    new object[] { false, false, true, new []  { null, null, new Point2D(-1, 01), new Point2D(01, 01), new Point2D(-1, 00), new Point2D(01, 00), new Point2D(-1, -1), new Point2D(01, -1) } },
+                    new object[] { false, true, false, new []  { null, null, new Point2D(01, -1), new Point2D(-1, -1), new Point2D(01, 00), new Point2D(-1, 00), new Point2D(01, 01), new Point2D(-1, 01) } },
+                    new object[] { false, true, true, new []   { null, null, new Point2D(01, 01), new Point2D(-1, 01), new Point2D( 1, 00), new Point2D(-1, 00), new Point2D(01, -1), new Point2D(-1, -1) } },
+                    new object[] { true, false, false, new []  { null, null, new Point2D(-1, -1), new Point2D(-1, 00), new Point2D(-1, 01), new Point2D(01, -1), new Point2D(01, 00), new Point2D(01, 01) } },
+                    new object[] { true, true, false, new []   { null, null, new Point2D(01, -1), new Point2D(01, 00), new Point2D(01, 01), new Point2D(-1, -1), new Point2D(-1, 00), new Point2D(-1, 01) } },
+                    new object[] { true, false, true, new []   { null, null, new Point2D(-1, 01), new Point2D(-1, 00), new Point2D(-1, -1), new Point2D(01, 01), new Point2D(01, 00), new Point2D(01, -1) } },
+                    new object[] { true, true, true, new []    { null, null, new Point2D(01, 01), new Point2D(01, 00), new Point2D(01, -1), new Point2D(-1, 01), new Point2D(-1, 00), new Point2D(-1, -1) } },
+                };
+            }
+        }
+    }
+}
---
+++ source/UnaryHeap.Utilities/UnaryHeap.Utilities/Point2DComparer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace UnaryHeap.Utilities
+{
+    /// <summary>
+    /// Defines a class that can be used to order Point2D objects.
+    /// </summary>
+    public class Point2DComparer : Comparer<Point2D>
+    {
+        #region Member Variables
+
+        bool sortFirstByX;
+        bool sortXDescending;
+        bool sortYDescending;
+
+        #endregion
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the UnaryHeap.Utilities.Point2DComparer class that sorts first in Y, then in X, both in ascending order.
+        /// </summary>
+        public Point2DComparer()
+            : this(false, false, false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the UnaryHeap.Utilities.Point2DComparer class with the specified sort ordering.
+        /// </summary>
+        /// <param name="sortFirstByX">If true, points will be sorted by their X coordinate value, and the Y coordinate will be used as a tiebreaker.
+        /// If false, points will be sorted by their Y coordinate value, and the X coordinate will be used as a tiebreaker.</param>
+        /// <param name="sortXDescending">If true, the X coordinates will be sorted in descending order instead of ascending order.</param>
+        /// <param name="sortYDescending">If true, the Y coordinates will be sorted in descending order instead of ascending order.</param>
+        public Point2DComparer(bool sortFirstByX, bool sortXDescending, bool sortYDescending)
+        {
+            this.sortFirstByX = sortFirstByX;
+            this.sortXDescending = sortXDescending;
+            this.sortYDescending = sortYDescending;
+        }
+
+        #endregion
+
+
+        #region Comparison
+
+        /// <summary>
+        /// Compares two UnaryHeap.Utilities.Point2D objects and returns a value indicating whether one is less than, equal to, or greater than the other.
+        /// </summary>
+        /// <param name="a">The first object to compare.</param>
+        /// <param name="b">The second object to compare.</param>
+        /// <returns>A negative integer, if x is less than y. Zero, if x equals y. A positive integer, if x is greater than y.</returns>
+        public override int Compare(Point2D a, Point2D b)
+        {
+            if (null == a && null == b)
+                return 0;
+            if (null == a)
+                return -1;
+            if (null == b)
+                return 1;
+
+            var xDiff = a.X.CompareTo(b.X);
+            var yDiff = a.Y.CompareTo(b.Y);
+
+            if (sortXDescending)
+                xDiff *= -1;
+            if (sortYDescending)
+                yDiff *= -1;
+
+            if (sortFirstByX)
+                return xDiff == 0 ? yDiff : xDiff;
+            else
+                return yDiff == 0 ? xDiff : yDiff;
+        }
+
+        #endregion
+    }
+}
