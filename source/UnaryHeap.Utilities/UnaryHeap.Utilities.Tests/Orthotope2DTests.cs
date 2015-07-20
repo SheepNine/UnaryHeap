a3aacf5637f2cc15a853cdc8382aa6ac97c611d6
SheepNine_cp <sheepnine@gmail.com>
2015-07-20 23:03:13 +0000

    Implement Orthotope2D class

---
+++ source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/Orthotope2DTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using UnaryHeap.Utilities;
+using Xunit;
+
+namespace UnaryHeap.Utilities.Tests
+{
+    public class Orthotope2DTests
+    {
+        [Fact]
+        public void Constructor_Values()
+        {
+            var sut = new Orthotope2D(1, 2, 3, 4);
+
+            Assert.Equal(1, sut.X.Min);
+            Assert.Equal(2, sut.Y.Min);
+            Assert.Equal(3, sut.X.Max);
+            Assert.Equal(4, sut.Y.Max);
+        }
+
+        [Fact]
+        public void Constructor_Ranges()
+        {
+            var sut = new Orthotope2D(new Range(1, 2), new Range(3, 4));
+
+            Assert.Equal(1, sut.X.Min);
+            Assert.Equal(2, sut.X.Max);
+            Assert.Equal(3, sut.Y.Min);
+            Assert.Equal(4, sut.Y.Max);
+        }
+
+        [Fact]
+        public void FromPoints()
+        {
+            var sut = Orthotope2D.FromPoints(new[] {
+                new Point2D(-7, 2),
+                new Point2D(-4, 3),
+                new Point2D(-1, 4),
+                new Point2D(02, 5),
+                new Point2D(05, 6),
+            });
+
+            Assert.Equal(-7, sut.X.Min);
+            Assert.Equal(5, sut.X.Max);
+            Assert.Equal(2, sut.Y.Min);
+            Assert.Equal(6, sut.Y.Max);
+        }
+
+        [Fact]
+        public void FromOnePoint()
+        {
+            var sut = Orthotope2D.FromPoints(new[] {
+                new Point2D(-7, 2),
+            });
+
+            Assert.Equal(-7, sut.X.Min);
+            Assert.Equal(-7, sut.X.Max);
+            Assert.Equal(2, sut.Y.Min);
+            Assert.Equal(2, sut.Y.Max);
+        }
+
+        [Fact]
+        public void Contains()
+        {
+            var sut = new Orthotope2D(0, 10, 10, 20);
+
+            Assert.True(sut.Contains(new Point2D(5, 15)));
+            Assert.False(sut.Contains(new Point2D(15, 15)));
+            Assert.False(sut.Contains(new Point2D(-5, 15)));
+            Assert.False(sut.Contains(new Point2D(5, 25)));
+            Assert.False(sut.Contains(new Point2D(5, 5)));
+        }
+
+        [Fact]
+        public void GetPadded()
+        {
+            var sut = new Orthotope2D(0, 10, 20, 30).GetPadded(3);
+
+            Assert.Equal(-3, sut.X.Min);
+            Assert.Equal(7, sut.Y.Min);
+            Assert.Equal(23, sut.X.Max);
+            Assert.Equal(33, sut.Y.Max);
+        }
+
+        [Fact]
+        public void GetScaled()
+        {
+            var sut = new Orthotope2D(-1, -3, 1, 3).GetScaled(2);
+
+            Assert.Equal(-2, sut.X.Min);
+            Assert.Equal(-6, sut.Y.Min);
+            Assert.Equal(2, sut.X.Max);
+            Assert.Equal(6, sut.Y.Max);
+        }
+
+
+        [Fact]
+        public void SimpleArgumentExceptions()
+        {
+            Assert.Throws<ArgumentNullException>("x", () => { new Orthotope2D(null, new Range(-1, 1)); });
+            Assert.Throws<ArgumentNullException>("y", () => { new Orthotope2D(new Range(-1, 1), null); });
+
+            Assert.Throws<ArgumentNullException>("value", () => { new Orthotope2D(0, 0, 0, 0).Contains(null); });
+            Assert.Throws<ArgumentNullException>("thickness", () => { new Orthotope2D(0, 0, 0, 0).GetPadded(null); });
+            Assert.Throws<ArgumentNullException>("factor", () => { new Orthotope2D(0, 0, 0, 0).GetScaled(null); });
+
+            Assert.Throws<ArgumentNullException>("points", () => { Orthotope2D.FromPoints(null); });
+            Assert.Throws<ArgumentNullException>("values", () => { Orthotope2D.FromPoints(new Point2D[] { null }); });
+            Assert.Throws<ArgumentException>(() => { Orthotope2D.FromPoints(new Point2D[] { }); });
+        }
+    }
+}
--- source/UnaryHeap.Utilities/UnaryHeap.Utilities/Orthotope2D.cs
+++ source/UnaryHeap.Utilities/UnaryHeap.Utilities/Orthotope2D.cs
@@ -1,6 +1,4 @@
-﻿#if INCLUDE_WORK_IN_PROGRESS
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace UnaryHeap.Utilities
@@ -10,6 +8,9 @@ namespace UnaryHeap.Utilities
     /// </summary>
     public class Orthotope2D
     {
+        Range x;
+        Range y;
+
         /// <summary>
         /// Intializes a new instance of the UnaryHeap.Utilities.Orthotope2D class.
         /// </summary>
@@ -18,7 +19,13 @@ namespace UnaryHeap.Utilities
         /// <exception cref="System.ArgumentNullException">x or y are null.</exception>
         public Orthotope2D(Range x, Range y)
         {
-            throw new NotImplementedException();
+            if (null == x)
+                throw new ArgumentNullException("x");
+            if (null == y)
+                throw new ArgumentNullException("y");
+
+            this.x = x;
+            this.y = y;
         }
 
         /// <summary>
@@ -31,8 +38,8 @@ namespace UnaryHeap.Utilities
         /// <exception cref="System.ArgumentNullException">minX, minY, maxX or maxY are null.</exception>
         /// <exception cref="System.ArgumentException">minX is greater than maxX, or minY is greater than maxY.</exception>
         public Orthotope2D(Rational minX, Rational minY, Rational maxX, Rational maxY)
+            : this(new Range(minX, maxX), new Range(minY, maxY))
         {
-            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -44,7 +51,11 @@ namespace UnaryHeap.Utilities
         /// <exception cref="System.ArgumentException">points is empty, or contains a null value.</exception>
         public static Orthotope2D FromPoints(IEnumerable<Point2D> points)
         {
-            throw new NotImplementedException();
+            if (null == points)
+                throw new ArgumentNullException("points");
+
+            var corners = Point2D.ComputeBoundingRectangleCorners(points);
+            return new Orthotope2D(corners.Item1.X, corners.Item1.Y, corners.Item2.X, corners.Item2.Y);
         }
 
         /// <summary>
@@ -52,7 +63,7 @@ namespace UnaryHeap.Utilities
         /// </summary>
         public Range X
         {
-            get { throw new NotImplementedException(); }
+            get { return x; }
         }
 
         /// <summary>
@@ -60,7 +71,7 @@ namespace UnaryHeap.Utilities
         /// </summary>
         public Range Y
         {
-            get { throw new NotImplementedException(); }
+            get { return y; }
         }
 
         /// <summary>
@@ -71,7 +82,10 @@ namespace UnaryHeap.Utilities
         /// <exception cref="System.ArgumentNullException">value is null.</exception>
         public bool Contains(Point2D value)
         {
-            throw new NotImplementedException();
+            if (null == value)
+                throw new ArgumentNullException("value");
+
+            return x.Contains(value.X) && y.Contains(value.Y);
         }
 
         /// <summary>
@@ -83,7 +97,7 @@ namespace UnaryHeap.Utilities
         /// <exception cref="System.ArgumentOutOfRangeException">thickness is negative and more than half of X.Size or Y.Size.</exception>
         public Orthotope2D GetPadded(Rational thickness)
         {
-            throw new NotImplementedException();
+            return new Orthotope2D(x.GetPadded(thickness), y.GetPadded(thickness));
         }
 
         /// <summary>
@@ -95,9 +109,7 @@ namespace UnaryHeap.Utilities
         /// <exception cref="System.ArgumentOutOfRangeException">factor is negative.</exception>
         public Orthotope2D GetScaled(Rational factor)
         {
-            throw new NotImplementedException();
+            return new Orthotope2D(x.GetScaled(factor), y.GetScaled(factor));
         }
     }
 }
-
-#endif
