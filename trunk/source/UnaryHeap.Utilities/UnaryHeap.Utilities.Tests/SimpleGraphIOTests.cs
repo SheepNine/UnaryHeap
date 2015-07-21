f965ef44c83f7061afe9691ece08330e93a72674
SheepNine_cp <SND\SheepNine_cp>
2015-07-21 03:13:36 +0000

    Work in progress on converting SimpleGraphs to/from JSON

---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/SimpleGraphIOTests.cs
@@ -0,0 +1,82 @@
+﻿#if INCLUDE_WORK_IN_PROGRESS
+
+using System;
+using System.IO;
+using UnaryHeap.Utilities;
+using Xunit;
+
+namespace UnaryHeap.Utilities.Tests
+{
+    public class SimpleGraphIOTests
+    {
+        [Fact]
+        public void EmptyDirectedGraph()
+        {
+            var text = "{\"directed\":true,\"vertex_count\":0,\"edges\":[]}";
+
+            RoundTripTest(text, (sut) =>
+            {
+                Assert.True(sut.IsDirected);
+                Assert.Equal(0, sut.NumVertices);
+                Assert.Empty(sut.Edges);
+            });
+        }
+
+        [Fact]
+        public void EmptyUndirectedGraph()
+        {
+            var text = "{\"directed\":false,\"vertex_count\":0,\"edges\":[]}";
+
+            RoundTripTest(text, (sut) =>
+            {
+                Assert.False(sut.IsDirected);
+                Assert.Equal(0, sut.NumVertices);
+                Assert.Empty(sut.Edges);
+            });
+        }
+
+        [Fact]
+        public void VertexOnlyGraph()
+        {
+            var text = "{\"directed\":true,\"vertex_count\":3,\"edges\":[]}";
+
+            RoundTripTest(text, (sut) =>
+            {
+                Assert.True(sut.IsDirected);
+                Assert.Equal(3, sut.NumVertices);
+                Assert.Empty(sut.Edges);
+            });
+        }
+
+        [Fact]
+        public void K2Graph()
+        {
+            var text = "{\"directed\":true,\"vertex_count\":2,\"edges\":[[0,1],[1,0]]}";
+
+            RoundTripTest(text, (sut) =>
+            {
+                Assert.True(sut.IsDirected);
+                Assert.Equal(2, sut.NumVertices);
+                Assert.Equal(new[] { Tuple.Create(0, 1), Tuple.Create(1, 0) }, sut.Edges);
+            });
+        }
+
+        void RoundTripTest(string text, Action<SimpleGraph> assertionCallback)
+        {
+            SimpleGraph sut;
+
+            using (var buffer = new StringReader(text))
+                sut = SimpleGraph.FromJson(buffer);
+
+            assertionCallback(sut);
+
+            using (var buffer = new StringWriter())
+            {
+                sut.ToJson(buffer);
+                Assert.Equal(text, buffer.ToString());
+            }
+        }
+    }
+}
+
+#endif
---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/SimpleGraphIO.cs
@@ -0,0 +1,80 @@
+﻿#if INCLUDE_WORK_IN_PROGRESS
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace UnaryHeap.Utilities
+{
+    public partial class SimpleGraph
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="output"></param>
+        public void ToJson(TextWriter output)
+        {
+            using (var writer = new JsonTextWriter(output))
+            {
+                writer.WriteStartObject();
+
+                writer.WritePropertyName("directed");
+                writer.WriteValue(IsDirected);
+
+                writer.WritePropertyName("vertex_count");
+                writer.WriteValue(NumVertices);
+
+                writer.WritePropertyName("edges");
+                writer.WriteStartArray();
+                
+                foreach (var i in Enumerable.Range(0, NumVertices))
+                    foreach (var j in adjacencies[i])
+                    {
+                        writer.WriteStartArray();
+                        writer.WriteValue(i);
+                        writer.WriteValue(j);
+                        writer.WriteEndArray();
+                    }
+
+                writer.WriteEndArray();
+
+                writer.WriteEndObject();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static SimpleGraph FromJson(TextReader input)
+        {
+            using (var reader = new JsonTextReader(input))
+                return new JsonSerializer().Deserialize<SimpleGraphPoco>(reader).Convert();
+        }
+
+        class SimpleGraphPoco
+        {
+            public bool directed { get; set; }
+            public int vertex_count { get; set; }
+            public int[][] edges { get; set; }
+
+            public SimpleGraph Convert()
+            {
+                var result = new SimpleGraph(directed);
+
+                foreach (var i in Enumerable.Range(0, vertex_count))
+                    result.AddVertex();
+
+                foreach (var edge in edges)
+                    result.AddEdge(edge[0], edge[1]);
+
+                return result;
+            }
+        }
+    }
+}
+
+#endif
