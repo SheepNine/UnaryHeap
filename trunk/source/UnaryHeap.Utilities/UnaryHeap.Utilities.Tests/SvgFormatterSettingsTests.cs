ea87434c0df5e936274d059629e996f841fae42a
SheepNine_cp <SND\SheepNine_cp>
2015-07-31 01:42:30 +0000

    Add SvgFormatterSettings class and tests

---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/SvgFormatterSettingsTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Xunit;
+
+namespace UnaryHeap.Utilities.Tests
+{
+    public class SvgFormatterSettingsTests
+    {
+        [Fact]
+        public void Defaults()
+        {
+            var sut = new SvgFormatterSettings();
+
+            Assert.Equal(640, sut.MajorAxisSize);
+            Assert.Equal(AxisOption.FromData, sut.MajorAxis);
+            Assert.Equal(50, sut.VertexDiameter);
+            Assert.Equal(15, sut.LineThickness);
+            Assert.Equal(5, sut.OutlineThickness);
+            Assert.Equal("lightgray", sut.BackgroundColor);
+            Assert.Equal("white", sut.VertexColor);
+            Assert.Equal("darkgray", sut.LineColor);
+            Assert.Equal("black", sut.OutlineColor);
+            Assert.True(sut.InvertYAxis);
+            Assert.True(sut.PadImage);
+        }
+
+        [Fact]
+        public void Mutators()
+        {
+            var sut = new SvgFormatterSettings();
+
+            sut.MajorAxisSize = 1640;
+            sut.MajorAxis = AxisOption.X;
+            sut.VertexDiameter = 150;
+            sut.LineThickness = 115;
+            sut.OutlineThickness = 15;
+            sut.BackgroundColor = "red";
+            sut.VertexColor = "green";
+            sut.LineColor = "blue";
+            sut.OutlineColor = "yellow";
+            sut.InvertYAxis = false;
+            sut.PadImage = false;
+
+            Assert.Equal(1640, sut.MajorAxisSize);
+            Assert.Equal(AxisOption.X, sut.MajorAxis);
+            Assert.Equal(150, sut.VertexDiameter);
+            Assert.Equal(115, sut.LineThickness);
+            Assert.Equal(15, sut.OutlineThickness);
+            Assert.Equal("red", sut.BackgroundColor);
+            Assert.Equal("green", sut.VertexColor);
+            Assert.Equal("blue", sut.LineColor);
+            Assert.Equal("yellow", sut.OutlineColor);
+            Assert.False(sut.InvertYAxis);
+            Assert.False(sut.PadImage);
+        }
+
+        [Fact]
+        public void PaddingThickness()
+        {
+            var sut = new SvgFormatterSettings();
+
+            Assert.Equal(30, sut.PaddingThickness);
+            sut.OutlineThickness = 0;
+            Assert.Equal(25, sut.PaddingThickness);
+            sut.VertexDiameter = 0;
+            Assert.Equal(new Rational(15, 2), sut.PaddingThickness);
+            sut.OutlineThickness = 5;
+            Assert.Equal(new Rational(25, 2), sut.PaddingThickness);
+            sut.PadImage = false;
+            Assert.Equal(0, sut.PaddingThickness);
+        }
+
+        [Fact]
+        public void SimpleArgumentExceptions()
+        {
+            var sut = new SvgFormatterSettings();
+
+            Assert.Throws<ArgumentNullException>("value", () => { sut.BackgroundColor = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.LineColor = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.LineThickness = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.MajorAxisSize = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.OutlineColor = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.OutlineThickness = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.VertexColor = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.VertexDiameter = null; });
+
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.BackgroundColor = string.Empty; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.LineColor = string.Empty; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.OutlineColor = string.Empty; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.VertexColor = string.Empty; });
+
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.LineThickness = -1; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.MajorAxisSize = 0; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.OutlineThickness = -1; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.VertexDiameter = -1; });
+        }
+    }
+}
---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/SvgFormatterSettings.cs
@@ -0,0 +1,218 @@
+﻿using System;
+
+namespace UnaryHeap.Utilities
+{
+    /// <summary>
+    /// Specifies color and size information for the UnaryHeap.Utilities.SvgGraph2DFormatter.
+    /// </summary>
+    public class SvgFormatterSettings
+    {
+        Rational majorAxisSize = 640;
+        /// <summary>
+        /// The width, in pixels, of the major axis of the output SVG image.
+        /// </summary>
+        public Rational MajorAxisSize
+        {
+            get { return majorAxisSize; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (0 >= value)
+                    throw new ArgumentOutOfRangeException("value", "value must be greater than zero.");
+
+                majorAxisSize = value;
+            }
+        }
+
+        AxisOption majorAxis = AxisOption.FromData;
+        /// <summary>
+        /// Specifies how the SVGWriter determines which axis to anchor to ImageSize.
+        /// </summary>
+        public AxisOption MajorAxis
+        {
+            get { return majorAxis; }
+            set { majorAxis = value; }
+        }
+
+        Rational vertexDiameter = 50;
+        /// <summary>
+        /// The diameter, in pixels, of vertices in the output SVG image.
+        /// </summary>
+        public Rational VertexDiameter
+        {
+            get { return vertexDiameter; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (0 > value)
+                    throw new ArgumentOutOfRangeException("value", "value must be greater than or equal to zero.");
+
+                vertexDiameter = value;
+            }
+        }
+
+        Rational lineThickness = 15;
+        /// <summary>
+        /// The thickness, in pixels, of edges in the output SVG image.
+        /// </summary>
+        public Rational LineThickness
+        {
+            get { return lineThickness; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (0 > value)
+                    throw new ArgumentOutOfRangeException("value", "value must be greater than or equal to zero.");
+
+                lineThickness = value;
+            }
+        }
+
+        Rational outlineThickness = 5;
+        /// <summary>
+        /// The thickness, in pixels, of the outline of vertices and edges in the output SVG image.
+        /// </summary>
+        public Rational OutlineThickness
+        {
+            get { return outlineThickness; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (0 > value)
+                    throw new ArgumentOutOfRangeException("value", "value must be greater than or equal to zero.");
+
+                outlineThickness = value;
+            }
+        }
+
+        string backgroundColor = "lightgray";
+        /// <summary>
+        /// The color used to fill the background.
+        /// </summary>
+        public string BackgroundColor
+        {
+            get { return backgroundColor; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (string.Empty == value)
+                    throw new ArgumentOutOfRangeException("value");
+
+                backgroundColor = value;
+            }
+        }
+
+        string vertexColor = "white";
+        /// <summary>
+        /// The color used to render vertices.
+        /// </summary>
+        public string VertexColor
+        {
+            get { return vertexColor; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (string.Empty == value)
+                    throw new ArgumentOutOfRangeException("value");
+
+                vertexColor = value;
+            }
+        }
+
+        string lineColor = "darkgray";
+        /// <summary>
+        /// The color used to render edges.
+        /// </summary>
+        public string LineColor
+        {
+            get { return lineColor; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (string.Empty == value)
+                    throw new ArgumentOutOfRangeException("value");
+
+                lineColor = value;
+            }
+        }
+
+        string outlineColor = "black";
+        /// <summary>
+        /// The color used to render vertex and edge outlines.
+        /// </summary>
+        public string OutlineColor
+        {
+            get { return outlineColor; }
+            set
+            {
+                if (null == value)
+                    throw new ArgumentNullException("value");
+                if (string.Empty == value)
+                    throw new ArgumentOutOfRangeException("value");
+
+                outlineColor = value;
+            }
+        }
+
+        bool invertYaxis = true;
+        /// <summary>
+        /// Whether to invert the Y axis so that the output image appears in a right-handed coordinate system.
+        /// </summary>
+        public bool InvertYAxis
+        {
+            get { return invertYaxis; }
+            set { invertYaxis = value; }
+        }
+
+        bool padImage = true;
+        /// <summary>
+        /// Whether to increase the output SVG view box so that vertcies/edges are not clipped off along the boundary of the image.
+        /// </summary>
+        public bool PadImage
+        {
+            get { return padImage; }
+            set { padImage = value; }
+        }
+
+        /// <summary>
+        /// Gets the size, in pixels, of the padding inside the image that prevents vertices and edges from being clipped by
+        /// the SVG viewport.
+        /// </summary>
+        public Rational PaddingThickness
+        {
+            get
+            {
+                if (false == padImage)
+                    return Rational.Zero;
+
+                return OutlineThickness + Rational.Max(VertexDiameter, LineThickness) / 2;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Specifies the axis used to convert image units to graph units.
+    /// </summary>
+    public enum AxisOption
+    {
+        /// <summary>
+        /// Always use the X axis.
+        /// </summary>
+        X,
+        /// <summary>
+        /// Always use the Y axis.
+        /// </summary>
+        Y,
+        /// <summary>
+        /// Use whichever axis has the largest range in graph units.
+        /// </summary>
+        FromData,
+    }
+}
