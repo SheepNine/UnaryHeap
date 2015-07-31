026501268172319e87c434af4b37d5a039f7e9c8
SheepNine_cp <SND\SheepNine_cp>
2015-07-31 02:08:59 +0000

    Rename 'line' to 'edge'

--- trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/SvgFormatterSettingsTests.cs
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/SvgFormatterSettingsTests.cs
@@ -13,11 +13,11 @@ namespace UnaryHeap.Utilities.Tests
             Assert.Equal(640, sut.MajorAxisSize);
             Assert.Equal(AxisOption.FromData, sut.MajorAxis);
             Assert.Equal(50, sut.VertexDiameter);
-            Assert.Equal(15, sut.LineThickness);
+            Assert.Equal(15, sut.EdgeThickness);
             Assert.Equal(5, sut.OutlineThickness);
             Assert.Equal("lightgray", sut.BackgroundColor);
             Assert.Equal("white", sut.VertexColor);
-            Assert.Equal("darkgray", sut.LineColor);
+            Assert.Equal("darkgray", sut.EdgeColor);
             Assert.Equal("black", sut.OutlineColor);
             Assert.True(sut.InvertYAxis);
             Assert.True(sut.PadImage);
@@ -31,11 +31,11 @@ namespace UnaryHeap.Utilities.Tests
             sut.MajorAxisSize = 1640;
             sut.MajorAxis = AxisOption.X;
             sut.VertexDiameter = 150;
-            sut.LineThickness = 115;
+            sut.EdgeThickness = 115;
             sut.OutlineThickness = 15;
             sut.BackgroundColor = "red";
             sut.VertexColor = "green";
-            sut.LineColor = "blue";
+            sut.EdgeColor = "blue";
             sut.OutlineColor = "yellow";
             sut.InvertYAxis = false;
             sut.PadImage = false;
@@ -43,11 +43,11 @@ namespace UnaryHeap.Utilities.Tests
             Assert.Equal(1640, sut.MajorAxisSize);
             Assert.Equal(AxisOption.X, sut.MajorAxis);
             Assert.Equal(150, sut.VertexDiameter);
-            Assert.Equal(115, sut.LineThickness);
+            Assert.Equal(115, sut.EdgeThickness);
             Assert.Equal(15, sut.OutlineThickness);
             Assert.Equal("red", sut.BackgroundColor);
             Assert.Equal("green", sut.VertexColor);
-            Assert.Equal("blue", sut.LineColor);
+            Assert.Equal("blue", sut.EdgeColor);
             Assert.Equal("yellow", sut.OutlineColor);
             Assert.False(sut.InvertYAxis);
             Assert.False(sut.PadImage);
@@ -75,8 +75,8 @@ namespace UnaryHeap.Utilities.Tests
             var sut = new SvgFormatterSettings();
 
             Assert.Throws<ArgumentNullException>("value", () => { sut.BackgroundColor = null; });
-            Assert.Throws<ArgumentNullException>("value", () => { sut.LineColor = null; });
-            Assert.Throws<ArgumentNullException>("value", () => { sut.LineThickness = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.EdgeColor = null; });
+            Assert.Throws<ArgumentNullException>("value", () => { sut.EdgeThickness = null; });
             Assert.Throws<ArgumentNullException>("value", () => { sut.MajorAxisSize = null; });
             Assert.Throws<ArgumentNullException>("value", () => { sut.OutlineColor = null; });
             Assert.Throws<ArgumentNullException>("value", () => { sut.OutlineThickness = null; });
@@ -84,11 +84,11 @@ namespace UnaryHeap.Utilities.Tests
             Assert.Throws<ArgumentNullException>("value", () => { sut.VertexDiameter = null; });
 
             Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.BackgroundColor = string.Empty; });
-            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.LineColor = string.Empty; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.EdgeColor = string.Empty; });
             Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.OutlineColor = string.Empty; });
             Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.VertexColor = string.Empty; });
 
-            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.LineThickness = -1; });
+            Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.EdgeThickness = -1; });
             Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.MajorAxisSize = 0; });
             Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.OutlineThickness = -1; });
             Assert.Throws<ArgumentOutOfRangeException>("value", () => { sut.VertexDiameter = -1; });
