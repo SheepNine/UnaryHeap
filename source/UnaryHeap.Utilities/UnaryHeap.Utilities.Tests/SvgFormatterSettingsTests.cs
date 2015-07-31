5bccb9d924362e0ac8fdc10e9ef1821d1349645b
SheepNine_cp <sheepnine@gmail.com>
2015-07-31 02:22:46 +0000

    Add SvgFormatterSettings constructor that reads values from a Graph2D's metadata

--- source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/SvgFormatterSettingsTests.cs
+++ source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/SvgFormatterSettingsTests.cs
@@ -24,6 +24,24 @@ namespace UnaryHeap.Utilities.Tests
         }
 
         [Fact]
+        public void DefaultsFromGraph()
+        {
+            var sut = new SvgFormatterSettings(new Graph2D(false));
+
+            Assert.Equal(640, sut.MajorAxisSize);
+            Assert.Equal(AxisOption.FromData, sut.MajorAxis);
+            Assert.Equal(50, sut.VertexDiameter);
+            Assert.Equal(15, sut.EdgeThickness);
+            Assert.Equal(5, sut.OutlineThickness);
+            Assert.Equal("lightgray", sut.BackgroundColor);
+            Assert.Equal("white", sut.VertexColor);
+            Assert.Equal("darkgray", sut.EdgeColor);
+            Assert.Equal("black", sut.OutlineColor);
+            Assert.True(sut.InvertYAxis);
+            Assert.True(sut.PadImage);
+        }
+
+        [Fact]
         public void Mutators()
         {
             var sut = new SvgFormatterSettings();
@@ -70,10 +88,56 @@ namespace UnaryHeap.Utilities.Tests
         }
 
         [Fact]
+        public void OverridesFromGraph()
+        {
+            var source = new Graph2D(false);
+            source.SetGraphMetadatum("major_axis_size", "200");
+            Assert.Equal(200, new SvgFormatterSettings(source).MajorAxisSize);
+
+            source.SetGraphMetadatum("major_axis", "X");
+            Assert.Equal(AxisOption.X, new SvgFormatterSettings(source).MajorAxis);
+            source.SetGraphMetadatum("major_axis", "x");
+            Assert.Equal(AxisOption.X, new SvgFormatterSettings(source).MajorAxis);
+
+            source.SetGraphMetadatum("major_axis", "Y");
+            Assert.Equal(AxisOption.Y, new SvgFormatterSettings(source).MajorAxis);
+            source.SetGraphMetadatum("major_axis", "y");
+            Assert.Equal(AxisOption.Y, new SvgFormatterSettings(source).MajorAxis);
+
+            source.SetGraphMetadatum("vertex_size", "100");
+            Assert.Equal(100, new SvgFormatterSettings(source).VertexDiameter);
+
+            source.SetGraphMetadatum("edge_size", "50"); 
+            Assert.Equal(50, new SvgFormatterSettings(source).EdgeThickness);
+
+            source.SetGraphMetadatum("outline_size", "6"); 
+            Assert.Equal(6, new SvgFormatterSettings(source).OutlineThickness);
+
+            source.SetGraphMetadatum("background_color", "pink"); 
+            Assert.Equal("pink", new SvgFormatterSettings(source).BackgroundColor);
+
+            source.SetGraphMetadatum("vertex_color", "purple");
+            Assert.Equal("purple", new SvgFormatterSettings(source).VertexColor);
+
+            source.SetGraphMetadatum("edge_color", "brown"); 
+            Assert.Equal("brown", new SvgFormatterSettings(source).EdgeColor);
+
+            source.SetGraphMetadatum("outline_color", "puce");
+            Assert.Equal("puce", new SvgFormatterSettings(source).OutlineColor);
+
+            source.SetGraphMetadatum("invert_y", "false"); 
+            Assert.False(new SvgFormatterSettings(source).InvertYAxis);
+
+            source.SetGraphMetadatum("pad", "false");
+            Assert.False(new SvgFormatterSettings(source).PadImage);
+        }
+
+        [Fact]
         public void SimpleArgumentExceptions()
         {
             var sut = new SvgFormatterSettings();
-
+            
+            Assert.Throws<ArgumentNullException>("source", () => { new SvgFormatterSettings(null); });
             Assert.Throws<ArgumentNullException>("value", () => { sut.BackgroundColor = null; });
             Assert.Throws<ArgumentNullException>("value", () => { sut.EdgeColor = null; });
             Assert.Throws<ArgumentNullException>("value", () => { sut.EdgeThickness = null; });
