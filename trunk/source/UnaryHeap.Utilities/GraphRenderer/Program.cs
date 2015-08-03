5cc87eee898f7a460a8d561efccd1a3db6f1bb69
SheepNine_cp <SND\SheepNine_cp>
2015-08-03 23:10:42 +0000

    Add GraphRenderer executable project

---
+++ trunk/source/UnaryHeap.Utilities/GraphRenderer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphRenderer
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return UnaryHeap.Utilities.Apps.GraphRendererApp.MainMethod(args);
+        }
+    }
+}
