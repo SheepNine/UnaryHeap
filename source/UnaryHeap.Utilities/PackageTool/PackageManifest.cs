2c55feb29862f976d7184cf40911afd8a7f806b2
SheepNine_cp <sheepnine@gmail.com>
2015-08-04 23:44:53 +0000

    Refactor PackageTool towards a general-purpose tool

---
+++ source/UnaryHeap.Utilities/PackageTool/PackageManifest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PackageTool
+{
+	class PackageManifest
+	{
+		public string OutputPath { get; private set; }
+		public PackageManifestEntry[] Entries { get; private set; }
+
+		public PackageManifest(string outputPath, IEnumerable<PackageManifestEntry> entries)
+		{
+			OutputPath = outputPath;
+			Entries = entries.ToArray();
+		}
+	}
+
+	class PackageManifestEntry
+	{
+		public string ArchivePath { get; private set; }
+		public string SourceFile { get; private set; }
+
+		public PackageManifestEntry(string archivePath, string sourceFile)
+		{
+			ArchivePath = archivePath;
+			SourceFile = sourceFile;
+		}
+	}
+}
