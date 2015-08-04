3da1cf66d058db6d643f469522427e9c02be769c
SheepNine_cp <SND\SheepNine_cp>
2015-08-04 23:44:53 +0000

    Refactor PackageTool towards a general-purpose tool

--- trunk/source/UnaryHeap.Utilities/PackageTool/Program.cs
+++ trunk/source/UnaryHeap.Utilities/PackageTool/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace PackageTool
 {
@@ -7,32 +10,14 @@ namespace PackageTool
 	{
 		static void Main(string[] args)
 		{
-			var outputArchive = @"..\..\..\packages\GraphRenderer.zip";
-
-			using (var file = File.Create(outputArchive))
-			using (var archive = new ZipArchive(file, ZipArchiveMode.Create))
-			{
-				PopulateEntry(archive,
-					@"..\..\GraphRenderer\Release\GraphRenderer.exe",
-					@"GraphRenderer\GraphRenderer.exe");
-				PopulateEntry(archive,
-					@"..\..\GraphRenderer\Release\Newtonsoft.Json.dll",
-					@"GraphRenderer\Newtonsoft.Json.dll");
-				PopulateEntry(archive,
-					@"..\..\GraphRenderer\Release\UnaryHeap.Utilities.dll",
-					@"GraphRenderer\UnaryHeap.Utilities.dll");
-			}
-		}
-
-		static void PopulateEntry(ZipArchive archive, string entryContents, string entryName)
-		{
-			var entry = archive.CreateEntry(entryName);
-
-			using (var entryStream = entry.Open())
-			using (var input = File.OpenRead(entryContents))
-			{
-				input.CopyTo(entryStream);
-			}
+			Packager.GeneratePackage(Environment.CurrentDirectory,
+				new PackageManifest(
+				@"..\..\..\packages\GraphRenderer.zip",
+				new[] {
+					new PackageManifestEntry(@"GraphRenderer\GraphRenderer.exe", @"..\..\GraphRenderer\Release\GraphRenderer.exe"),
+					new PackageManifestEntry(@"GraphRenderer\Newtonsoft.Json.dll", @"..\..\GraphRenderer\Release\Newtonsoft.Json.dll"),
+					new PackageManifestEntry(@"GraphRenderer\UnaryHeap.Utilities.dll", @"..\..\GraphRenderer\Release\UnaryHeap.Utilities.dll"),
+				}));
 		}
 	}
 }
