71a6bec013232cd53b89f72f18faa85ad672085a
SheepNine_cp <sheepnine@gmail.com>
2015-08-04 03:56:29 +0000

    Create proof-of-concept release packager

---
+++ source/UnaryHeap.Utilities/PackageTool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace PackageTool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var outputArchive = @"..\..\..\packages\GraphRenderer.zip";
+
+			using (var file = File.Create(outputArchive))
+			using (var archive = new ZipArchive(file, ZipArchiveMode.Create))
+			{
+				PopulateEntry(archive,
+					@"..\..\GraphRenderer\Release\GraphRenderer.exe",
+					@"GraphRenderer\GraphRenderer.exe");
+				PopulateEntry(archive,
+					@"..\..\GraphRenderer\Release\Newtonsoft.Json.dll",
+					@"GraphRenderer\Newtonsoft.Json.dll");
+				PopulateEntry(archive,
+					@"..\..\GraphRenderer\Release\UnaryHeap.Utilities.dll",
+					@"GraphRenderer\UnaryHeap.Utilities.dll");
+			}
+		}
+
+		static void PopulateEntry(ZipArchive archive, string entryContents, string entryName)
+		{
+			var entry = archive.CreateEntry(entryName);
+
+			using (var entryStream = entry.Open())
+			using (var input = File.OpenRead(entryContents))
+			{
+				input.CopyTo(entryStream);
+			}
+		}
+	}
+}
