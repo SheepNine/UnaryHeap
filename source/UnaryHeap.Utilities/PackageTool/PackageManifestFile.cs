4ebe751096f05b141b46c8c71ea446b701242fc9
SheepNine_cp <sheepnine@gmail.com>
2015-08-05 00:08:23 +0000

    Last step of PackageTool refactoring

--- source/UnaryHeap.Utilities/PackageTool/PackageManifestFile.cs
+++ source/UnaryHeap.Utilities/PackageTool/PackageManifestFile.cs
@@ -1,45 +1,34 @@
 ﻿using System.Collections.Generic;
-using System.IO;
-using System.Xml;
 using System.Linq;
+using System.Xml;
 
 namespace PackageTool
 {
 	static class PackageManifestFile
 	{
-		const string example =
-@"<PackageManifest OutputPath=""..\..\..\packages\GraphRenderer.zip"">
-	<Entry ArchivePath=""GraphRenderer\GraphRenderer.exe"" SourceFile=""..\..\GraphRenderer\Release\GraphRenderer.exe""/>
-	<Entry ArchivePath=""GraphRenderer\Newtonsoft.Json.dll"" SourceFile=""..\..\GraphRenderer\Release\Newtonsoft.Json.dll""/>
-	<Entry ArchivePath=""GraphRenderer\UnaryHeap.Utilities.dll"" SourceFile=""..\..\GraphRenderer\Release\UnaryHeap.Utilities.dll""/>
-</PackageManifest>
-";
 		public static PackageManifest Parse(string filename)
 		{
 			var doc = new XmlDocument();
-			using (var reader = new StringReader(example))
-				doc.Load(reader);
+			doc.Load(filename);
 
-			return new PackageManifest(
-				ParseOutputPath(doc),
-				ParseEntries(doc));
+			return new PackageManifest(ParseOutputPath(doc), ParseEntries(doc));
 		}
 
-		private static string ParseOutputPath(XmlDocument doc)
+		static string ParseOutputPath(XmlDocument doc)
 		{
-			return ((XmlElement)doc.SelectSingleNode("/PackageManifest")).GetAttribute("OutputPath");
+			return ((XmlElement)doc.SelectSingleNode("/ArchiveManifest")).GetAttribute("OutputFileName");
 		}
 
-		private static IEnumerable<PackageManifestEntry> ParseEntries(XmlDocument doc)
+		static IEnumerable<PackageManifestEntry> ParseEntries(XmlDocument doc)
 		{
-			return doc.SelectNodes("/PackageManifest/Entry").Cast<XmlElement>().Select(entry => ParseEntry(entry));
+			return doc.SelectNodes("/ArchiveManifest/Entry").Cast<XmlElement>().Select(entry => ParseEntry(entry));
 		}
 
-		private static PackageManifestEntry ParseEntry(XmlElement entry)
+		static PackageManifestEntry ParseEntry(XmlElement entry)
 		{
 			return new PackageManifestEntry(
-				entry.GetAttribute("ArchivePath"),
-				entry.GetAttribute("SourceFile")
+				entry.GetAttribute("ArchiveName"),
+				entry.GetAttribute("SourceFileName")
 			);
 		}
 	}
