be735a33fbdaf75704c42da69b266fa95e6e945b
SheepNine_cp <SND\SheepNine_cp>
2015-08-05 03:26:15 +0000

    Include blank documentation in GraphRenderer package

---
+++ trunk/source/UnaryHeap.Utilities/GraphRenderer/deployables/tryme.bat
@@ -0,0 +1,5 @@
+@REM ----------------------------------------------------------
+@REM Run with one argument to auto-generate the output filename
+@REM ----------------------------------------------------------
+
+..\Bin\GraphRenderer.exe .\rainbow.json
