bb5c68ecde214730b42b4160989760a4a98db4a8
SheepNine_cp <sheepnine@gmail.com>
2015-08-11 02:58:47 +0000

    Add a second example JSON file to GraphRenderer package and tests

--- source/UnaryHeap.Utilities/GraphRenderer/deployables/tryme.bat
+++ source/UnaryHeap.Utilities/GraphRenderer/deployables/tryme.bat
@@ -1,5 +1,11 @@
-@REM ----------------------------------------------------------
-@REM Run with one argument to auto-generate the output filename
-@REM ----------------------------------------------------------
+@echo ----------------------------------------------------------
+@echo Run with one argument to auto-generate the output filename
+@echo ----------------------------------------------------------
 
 ..\Bin\GraphRenderer.exe .\rainbow.json
+
+@echo ----------------------------------------------------------
+@echo Run with one two arguments to specify the output filename
+@echo ----------------------------------------------------------
+
+..\Bin\GraphRenderer.exe .\unaryheap.json .\unaryheap_output.svg
