3c941faa02618b863ecb4f4a565b05c49b735f09
SheepNine_cp <SND\SheepNine_cp>
2015-09-08 22:01:04 +0000

    Add new project for application to generate random mazes

---
+++ trunk/source/UnaryHeap.Utilities/MazeGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnaryHeap.Utilities.D2;
+
+namespace MazeGenerator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (2 == args.Length)
+            {
+                return Run(int.Parse(args[0]), args[1]);
+            }
+            else if (3 == args.Length)
+            {
+                return Run(int.Parse(args[0]), int.Parse(args[1]), args[2]);
+            }
+            else
+            {
+                Console.Error.WriteLine("Incorrect number of arguments.");
+                return 1;
+            }
+        }
+
+        static int Run(int size, string outputFilename)
+        {
+            return Run(Point2D.GenerateRandomPoints(size), outputFilename);
+        }
+
+        static int Run(int size, int seed, string outputFilename)
+        {
+            return Run(Point2D.GenerateRandomPoints(size, seed), outputFilename);
+        }
+
+        static int Run(Point2D[] sites, string outputFilename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
