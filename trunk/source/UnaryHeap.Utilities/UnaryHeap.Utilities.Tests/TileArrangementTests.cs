1ea5d20e60372f682c425898e0bb59e99f3d83cf
SheepNine_cp <SND\SheepNine_cp>
2015-09-25 03:33:04 +0000

    Add serialization for TileArrangement class

--- trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/TileArrangementTests.cs
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/TileArrangementTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using UnaryHeap.Utilities.Misc;
 using Xunit;
@@ -24,6 +25,48 @@ namespace UnaryHeap.Utilities.Tests
                     sut[x, y] = replacement;
                     Assert.Equal(replacement, sut[x, y]);
                 }
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var sut = new TileArrangement(4, 3);
+
+            foreach (var x in Enumerable.Range(0, 4))
+                foreach (var y in Enumerable.Range(0, 3))
+                    sut[x, y] = x * 10 + y;
+
+            var buffer = new MemoryStream();
+            sut.Serialize(buffer);
+            buffer.Seek(0, SeekOrigin.Begin);
+            var sut2 = TileArrangement.Deserialize(buffer);
+
+            Assert.Equal(4, sut2.TileCountX);
+            Assert.Equal(3, sut2.TileCountY);
+
+            foreach (var x in Enumerable.Range(0, 4))
+                foreach (var y in Enumerable.Range(0, 3))
+                    Assert.Equal(x * 10 + y, sut2[x, y]);
+
+            Assert.Equal(new byte[] {
+                4, 0, 0, 0,
+                3, 0, 0, 0,
+                   
+                0, 0, 0, 0,
+                10, 0, 0, 0,
+                20, 0, 0, 0,
+                30, 0, 0, 0,
+                   
+                1, 0, 0, 0,
+                11, 0, 0, 0,
+                21, 0, 0, 0,
+                31, 0, 0, 0,
+                   
+                2, 0, 0, 0,
+                12, 0, 0, 0,
+                22, 0, 0, 0,
+                32, 0, 0, 0,
+            }, buffer.ToArray());
         }
 
         [Fact]
