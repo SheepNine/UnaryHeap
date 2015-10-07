b421fc3841636792d5208c0f620d779096b9e50a
SheepNine_cp <sheepnine@gmail.com>
2015-10-07 03:39:11 +0000

    Refactor in IMazeLayout interface

--- source/UnaryHeap.Utilities/MazeGenerator/Program.cs
+++ source/UnaryHeap.Utilities/MazeGenerator/Program.cs
@@ -13,11 +13,17 @@ namespace MazeGenerator
         {
             if (2 == args.Length)
             {
-                return Run(int.Parse(args[0]), args[1]);
+                return Run(
+                    new VoronoiMazeLayout(int.Parse(args[0])),
+                    new BiggestWallEdgeWeightAssignment(),
+                    args[1]);
             }
             else if (3 == args.Length)
             {
-                return Run(int.Parse(args[0]), int.Parse(args[1]), args[2]);
+                return Run(
+                    new VoronoiMazeLayout(int.Parse(args[0]), int.Parse(args[1])),
+                    new BiggestWallEdgeWeightAssignment(),
+                    args[2]);
             }
             else
             {
@@ -26,69 +32,44 @@ namespace MazeGenerator
             }
         }
 
-        static int Run(int size, string outputFilename)
+        static int Run(
+            IMazeLayout layout, IEdgeWeightAssignment edgeWeights, string outputFilename)
         {
             Graph2D physicalGraph, logicalGraph;
-            MakeVoronoiDelaunayGraphs(size, out physicalGraph, out logicalGraph);
-            MakeShortWallsImpassable(logicalGraph, physicalGraph, new Rational(size, 100), false);
-            return Run(physicalGraph, logicalGraph, outputFilename);
+            layout.Generate(out physicalGraph, out logicalGraph);
+
+            return Run(physicalGraph, logicalGraph, edgeWeights, outputFilename);
         }
 
-        static int Run(int size, int seed, string outputFilename)
-        {
-            Graph2D physicalGraph, logicalGraph;
-            MakeVoronoiDelaunayGraphs(size, out physicalGraph, out logicalGraph, seed);
-            MakeShortWallsImpassable(logicalGraph, physicalGraph, new Rational(size, 100), false);
-            return Run(physicalGraph, logicalGraph, outputFilename);
-        }
-
-        static int Run(Graph2D physicalGraph, Graph2D logicalGraph, string outputFilename)
+        static int Run(
+            Graph2D physicalGraph, Graph2D logicalGraph,
+            IEdgeWeightAssignment edgeWeights, string outputFilename)
         {
             MazeConnector.ConnectRooms(
-                logicalGraph, physicalGraph,
-                new BiggestWallEdgeWeightAssignment(), true, false);
+                logicalGraph, physicalGraph, edgeWeights, true, false);
 
             using (var output = File.CreateText(outputFilename))
                 MazeWriter.WriteMaze(output, physicalGraph);
 
             return 0;
         }
+    }
 
-        static void MakeShortWallsImpassable(
-            Graph2D logicalGraph, Graph2D physicalGraph, Rational minLength, bool highlight)
+    interface IMazeLayout
+    {
+        void Generate(out Graph2D physicalGraph, out Graph2D logicalGraph);
+    }
+
+    class LatticeMazeLayout : IMazeLayout
+    {
+        int size;
+
+        public LatticeMazeLayout(int size)
         {
-            var minLengthSquared = minLength.Squared;
-
-            foreach (var edge in logicalGraph.Edges.ToArray())
-            {
-                var dual = logicalGraph.GetDualEdge(edge.Item1, edge.Item2);
-
-                if (Point2D.Quadrance(dual.Item1, dual.Item2) < minLengthSquared)
-                {
-                    logicalGraph.RemoveEdge(edge.Item1, edge.Item2);
-
-                    if (highlight)
-                        physicalGraph.SetEdgeMetadatum(
-                            dual.Item1, dual.Item2, "color", "#80FF80");
-                }
-            }
+            this.size = size;
         }
 
-        static void MakeVoronoiDelaunayGraphs(
-            int size, out Graph2D physicalGraph, out Graph2D logicalGraph,
-            int? seed = null)
-        {
-            var sites = Point2D.GenerateRandomPoints(size, seed);
-
-            var listener = new MazeListener();
-            FortunesAlgorithm.Execute(sites, listener);
-
-            physicalGraph = listener.PhysicalGraph;
-            logicalGraph = listener.LogicalGraph;
-        }
-
-        static void MakeSquareLatticeGraphs(
-            int size, out Graph2D physicalGraph, out Graph2D logicalGraph)
+        public void Generate(out Graph2D physicalGraph, out Graph2D logicalGraph)
         {
             physicalGraph = new Graph2D(false);
             logicalGraph = new Graph2D(false);
@@ -127,7 +108,6 @@ namespace MazeGenerator
                         logicalGraph.AddEdge(
                             new Point2D(x + oneHalf, y + oneHalf),
                             new Point2D(x + oneHalf, y - oneHalf));
-
                         logicalGraph.SetDualEdge(
                             new Point2D(x + oneHalf, y + oneHalf),
                             new Point2D(x + oneHalf, y - oneHalf),
@@ -135,6 +115,52 @@ namespace MazeGenerator
                             new Point2D(x + 1, y));
                     }
                 }
+        }
+    }
+
+    class VoronoiMazeLayout : IMazeLayout
+    {
+        int size;
+        int? seed;
+
+        public VoronoiMazeLayout(int size, int? seed = null)
+        {
+            this.size = size;
+            this.seed = seed;
+        }
+
+        public void Generate(out Graph2D physicalGraph, out Graph2D logicalGraph)
+        {
+            var sites = Point2D.GenerateRandomPoints(size, seed);
+
+            var listener = new MazeListener();
+            FortunesAlgorithm.Execute(sites, listener);
+
+            physicalGraph = listener.PhysicalGraph;
+            logicalGraph = listener.LogicalGraph;
+
+            MakeShortWallsImpassable(
+                logicalGraph, physicalGraph, new Rational(size, 100), false);
+        }
+
+        static void MakeShortWallsImpassable(
+            Graph2D logicalGraph, Graph2D physicalGraph, Rational minLength, bool highlight)
+        {
+            var minLengthSquared = minLength.Squared;
+
+            foreach (var edge in logicalGraph.Edges.ToArray())
+            {
+                var dual = logicalGraph.GetDualEdge(edge.Item1, edge.Item2);
+
+                if (Point2D.Quadrance(dual.Item1, dual.Item2) < minLengthSquared)
+                {
+                    logicalGraph.RemoveEdge(edge.Item1, edge.Item2);
+
+                    if (highlight)
+                        physicalGraph.SetEdgeMetadatum(
+                            dual.Item1, dual.Item2, "color", "#80FF80");
+                }
+            }
         }
     }
 }
