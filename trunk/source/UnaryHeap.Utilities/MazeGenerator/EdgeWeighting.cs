96afbe48d58763bb711cc2a198ce3ac402597a3b
SheepNine_cp <SND\SheepNine_cp>
2015-10-07 03:23:33 +0000

    Refactor edge weighting classes into better code files

---
+++ trunk/source/UnaryHeap.Utilities/MazeGenerator/EdgeWeighting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnaryHeap.Utilities.Core;
+using UnaryHeap.Utilities.D2;
+
+namespace MazeGenerator
+{
+    interface IEdgeWeightAssignment
+    {
+        Rational AssignEdgeWeight(
+            Point2D l1, Point2D l2, Point2D p1, Point2D p2);
+    }
+
+    class BiggestWallEdgeWeightAssignment : IEdgeWeightAssignment
+    {
+        public Rational AssignEdgeWeight(Point2D l1, Point2D l2, Point2D p1, Point2D p2)
+        {
+            return -Point2D.Quadrance(p1, p2);
+        }
+    }
+
+    class RandomEdgeWeightAssignment : IEdgeWeightAssignment
+    {
+        Random random;
+
+        public RandomEdgeWeightAssignment(int? seed = null)
+        {
+            if (seed.HasValue)
+                random = new Random(seed.Value);
+            else
+                random = new Random();
+        }
+
+        public Rational AssignEdgeWeight(Point2D l1, Point2D l2, Point2D p1, Point2D p2)
+        {
+            return random.Next(100);
+        }
+    }
+}
