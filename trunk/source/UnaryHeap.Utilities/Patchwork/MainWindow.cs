6f74a5025ea455e7eebe4eb8384f17f9b2f0d51c
SheepNine_cp <SND\SheepNine_cp>
2015-10-25 04:10:14 +0000

    Create new application 'Patchwork' and integrate with build system

---
+++ trunk/source/UnaryHeap.Utilities/Patchwork/MainWindow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Patchwork
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
