d5c3e853303320ce1ad77428c33c8280e168531c
SheepNine_cp <SND\SheepNine_cp>
2015-10-25 17:44:00 +0000

    Set up Patchwork application with default data files; save/load to come later

--- trunk/source/UnaryHeap.Utilities/Patchwork/Program.cs
+++ trunk/source/UnaryHeap.Utilities/Patchwork/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
+using UnaryHeap.Utilities.Misc;
 
 namespace Patchwork
 {
@@ -11,10 +14,73 @@ namespace Patchwork
         [STAThread]
         static void Main()
         {
+            // --- Set up ---
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ProgramData.CreateDefaultArrangement();
+
+
+            // --- Load data ---
+
+            var arrangement = ProgramData.LoadArrangement();
+            var tileset = ProgramData.LoadTileset();
+
+
+            // --- Execute program ---
 
             Application.Run(new MainWindow());
+
+            // --- Save data ---
+
+            ProgramData.SaveArrangement(arrangement);
+            tileset.Dispose();
         }
+    }
+
+    static class ProgramData
+    {
+        #region Arrangement
+
+        static string DefaultArrangementFile
+        {
+            get { return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "patchwork.arr"); }
+        }
+
+        public static void CreateDefaultArrangement()
+        {
+            if (File.Exists(DefaultArrangementFile))
+                return;
+
+            SaveArrangement(new TileArrangement(64, 48));
+        }
+
+        public static void SaveArrangement(TileArrangement defaultContents)
+        {
+            using (var file = File.Create(DefaultArrangementFile))
+                defaultContents.Serialize(file);
+        }
+
+        public static TileArrangement LoadArrangement()
+        {
+            using (var file = File.Open(DefaultArrangementFile, FileMode.Open))
+                return TileArrangement.Deserialize(file);
+        }
+
+        #endregion
+
+
+        #region Tileset
+
+        static string DefaultTileImageFile
+        {
+            get { return "tileset_template_1x.png"; }
+        }
+
+        public static Tileset LoadTileset()
+        {
+            return new Tileset(new Bitmap(DefaultTileImageFile), 8);
+        }
+
+        #endregion
     }
 }
