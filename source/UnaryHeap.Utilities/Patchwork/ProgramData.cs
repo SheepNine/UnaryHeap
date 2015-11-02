5f01d697cbf9864dec45b00010f26e910babf036
SheepNine_cp <sheepnine@gmail.com>
2015-11-02 22:04:31 +0000

    Dead code removal

--- source/UnaryHeap.Utilities/Patchwork/ProgramData.cs
+++ source/UnaryHeap.Utilities/Patchwork/ProgramData.cs
@@ -5,44 +5,8 @@ using UnaryHeap.Utilities.Misc;
 
 namespace Patchwork
 {
-
     static class ProgramData
     {
-        #region Arrangement
-
-        static string DefaultArrangementFile
-        {
-            get
-            {
-                return Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                    "patchwork.arr");
-            }
-        }
-
-        public static void CreateDefaultArrangement()
-        {
-            if (File.Exists(DefaultArrangementFile))
-                return;
-
-            SaveArrangement(new TileArrangement(64, 48));
-        }
-
-        public static void SaveArrangement(TileArrangement defaultContents)
-        {
-            using (var file = File.Create(DefaultArrangementFile))
-                defaultContents.Serialize(file);
-        }
-
-        public static TileArrangement LoadArrangement()
-        {
-            using (var file = File.Open(DefaultArrangementFile, FileMode.Open))
-                return TileArrangement.Deserialize(file);
-        }
-
-        #endregion
-
-
         #region Tileset
 
         static string DefaultTileImageFile
