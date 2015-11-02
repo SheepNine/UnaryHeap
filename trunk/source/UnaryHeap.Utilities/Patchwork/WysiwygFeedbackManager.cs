bb3184f61e83798296dfc6c542924ab457ddc3b2
SheepNine_cp <SND\SheepNine_cp>
2015-11-02 22:19:28 +0000

    Add WysiwygFeedbackManager class to cut down on screen refreshes by only invalidating the panel for visible changes to the feedback

---
+++ trunk/source/UnaryHeap.Utilities/Patchwork/WysiwygFeedbackManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using UnaryHeap.Utilities.UI;
+
+namespace Patchwork
+{
+    interface IWysiwygFeedback : IEquatable<IWysiwygFeedback>
+    {
+        void Render(Graphics g, Rectangle clipRectangle);
+    }
+
+    class NullWysiwygFeedback : IWysiwygFeedback
+    {
+        public void Render(Graphics g, Rectangle clipRectangle)
+        {
+        }
+
+        public bool Equals(IWysiwygFeedback other)
+        {
+            return (null != (other as NullWysiwygFeedback));
+        }
+    }
+
+    class WysiwygFeedbackManager
+    {
+        WysiwygPanel target;
+        IWysiwygFeedback currentFeedback = new NullWysiwygFeedback();
+
+        public WysiwygFeedbackManager(WysiwygPanel panel)
+        {
+            target = panel;
+            target.PaintFeedback += target_PaintFeedback;
+        }
+
+        void target_PaintFeedback(object sender, PaintEventArgs e)
+        {
+            currentFeedback.Render(e.Graphics, e.ClipRectangle);
+        }
+
+        public void ClearFeedback()
+        {
+            SetFeedback(new NullWysiwygFeedback());
+        }
+
+        public void SetFeedback(IWysiwygFeedback newFeedback)
+        {
+            if (null == newFeedback)
+                newFeedback = new NullWysiwygFeedback();
+
+            if (false == currentFeedback.Equals(newFeedback))
+            {
+                currentFeedback = newFeedback;
+                target.InvalidateFeedback();
+            }
+        }
+    }
+}
