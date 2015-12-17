d0c0cf6e92792c6468537c37ee78e1153a5418a7
SheepNine_cp <SND\SheepNine_cp>
2015-12-17 01:56:45 +0000

    Relocate mapping classes to UnaryHeap.Utilities

--- trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/LinearMapping2DTests.cs
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/LinearMapping2DTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnaryHeap.Utilities.D2;
+using UnaryHeap.Utilities.Misc;
 using Xunit;
 
 namespace UnaryHeap.Utilities.Tests
@@ -15,8 +16,8 @@ namespace UnaryHeap.Utilities.Tests
             var dst2 = new Point2D(1, 1);
 
             // The order that the points are specified should not affect the results
-            var sut1 = LinearMapping.From(src1, src2).To(dst1, dst2);
-            var sut2 = LinearMapping.From(src2, src1).To(dst2, dst1);
+            var sut1 = LinearMapping.From(src1, src2).Onto(dst1, dst2);
+            var sut2 = LinearMapping.From(src2, src1).Onto(dst2, dst1);
 
             Assert.Equal(dst1, sut1 * src1);
             Assert.Equal(dst2, sut1 * src2);
@@ -40,8 +41,8 @@ namespace UnaryHeap.Utilities.Tests
             var dst2 = new Point2D(2, 2);
 
             // The order that the points are specified should not affect the results
-            var sut1 = LinearMapping.From(src1, src2).To(dst1, dst2);
-            var sut2 = LinearMapping.From(src2, src1).To(dst2, dst1);
+            var sut1 = LinearMapping.From(src1, src2).Onto(dst1, dst2);
+            var sut2 = LinearMapping.From(src2, src1).Onto(dst2, dst1);
 
             Assert.Equal(dst1, sut1 * src1);
             Assert.Equal(dst2, sut1 * src2);
@@ -64,9 +65,9 @@ namespace UnaryHeap.Utilities.Tests
             var from = LinearMapping.From(new Point2D(1, 0), new Point2D(0, 1));
 
             Assert.Throws<ArgumentNullException>("dst1",
-                () => { from.To(null, Point2D.Origin); });
+                () => { from.Onto(null, Point2D.Origin); });
             Assert.Throws<ArgumentNullException>("dst2",
-                () => { from.To(Point2D.Origin, null); });
+                () => { from.Onto(Point2D.Origin, null); });
 
             Assert.Equal("Source points are linearly dependent; cannot invert.",
                 Assert.Throws<ArgumentException>(
