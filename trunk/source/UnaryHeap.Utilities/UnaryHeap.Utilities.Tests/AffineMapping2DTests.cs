0cb9878d8939eba3957cc0abd5acc29f7338f7fa
SheepNine_cp <SND\SheepNine_cp>
2015-12-18 02:29:14 +0000

    Implement and test 3D linear/2D affine mapping

--- trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/AffineMapping2DTests.cs
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/AffineMapping2DTests.cs
@@ -1,5 +1,4 @@
-﻿#if INCLUDE_WORK_IN_PROGRESS
-
+﻿using System;
 using UnaryHeap.Utilities.D2;
 using UnaryHeap.Utilities.Misc;
 using Xunit;
@@ -9,7 +8,7 @@ namespace UnaryHeap.Utilities.Tests
     public class AffineMapping2DTests
     {
         [Fact]
-        public void TwoDimensions()
+        public void NonSingularResult()
         {
             var src1 = new Point2D(1, 2);
             var src2 = new Point2D(4, 5);
@@ -21,12 +20,12 @@ namespace UnaryHeap.Utilities.Tests
 
             var suts = new[]
             {
-                AffineMapping.From(src1, src2, src3).To(dst1, dst2, dst3),
-                AffineMapping.From(src1, src3, src2).To(dst1, dst3, dst2),
-                AffineMapping.From(src3, src1, src2).To(dst3, dst1, dst2),
-                AffineMapping.From(src3, src2, src1).To(dst3, dst2, dst1),
-                AffineMapping.From(src2, src3, src1).To(dst2, dst3, dst1),
-                AffineMapping.From(src2, src1, src3).To(dst2, dst1, dst3),
+                AffineMapping.From(src1, src2, src3).Onto(dst1, dst2, dst3),
+                AffineMapping.From(src1, src3, src2).Onto(dst1, dst3, dst2),
+                AffineMapping.From(src3, src1, src2).Onto(dst3, dst1, dst2),
+                AffineMapping.From(src3, src2, src1).Onto(dst3, dst2, dst1),
+                AffineMapping.From(src2, src3, src1).Onto(dst2, dst3, dst1),
+                AffineMapping.From(src2, src1, src3).Onto(dst2, dst1, dst3),
             };
 
             foreach (var sut in suts)
@@ -36,7 +35,55 @@ namespace UnaryHeap.Utilities.Tests
                 Assert.Equal(dst3, (sut * src3.Homogenized()).Dehomogenized());
             }
         }
+
+        [Fact]
+        public void SingularResult()
+        {
+            var src1 = new Point2D(1, 2);
+            var src2 = new Point2D(4, 5);
+            var src3 = new Point2D(7, 9);
+
+            var dst1 = new Point2D(2, 2);
+            var dst2 = new Point2D(2, 2);
+            var dst3 = new Point2D(2, 2);
+
+            var suts = new[]
+            {
+                AffineMapping.From(src1, src2, src3).Onto(dst1, dst2, dst3),
+                AffineMapping.From(src1, src3, src2).Onto(dst1, dst3, dst2),
+                AffineMapping.From(src3, src1, src2).Onto(dst3, dst1, dst2),
+                AffineMapping.From(src3, src2, src1).Onto(dst3, dst2, dst1),
+                AffineMapping.From(src2, src3, src1).Onto(dst2, dst3, dst1),
+                AffineMapping.From(src2, src1, src3).Onto(dst2, dst1, dst3),
+            };
+
+            foreach (var sut in suts)
+            {
+                Assert.Equal(dst1, (sut * src1.Homogenized()).Dehomogenized());
+                Assert.Equal(dst2, (sut * src2.Homogenized()).Dehomogenized());
+                Assert.Equal(dst3, (sut * src3.Homogenized()).Dehomogenized());
+            }
+        }
+
+        [Fact]
+        public void SimpleArgumentExceptions()
+        {
+            Assert.Throws<ArgumentNullException>("src1",
+                () => { AffineMapping.From(null, Point2D.Origin, Point2D.Origin); });
+            Assert.Throws<ArgumentNullException>("src2",
+                () => { AffineMapping.From(Point2D.Origin, null, Point2D.Origin); });
+            Assert.Throws<ArgumentNullException>("src3",
+                () => { AffineMapping.From(Point2D.Origin, Point2D.Origin, null); });
+
+            var sut = AffineMapping.From(
+                new Point2D(1, 0), new Point2D(0, 1), new Point2D(0, 0));
+
+            Assert.Throws<ArgumentNullException>("dst1",
+                () => { sut.Onto(null, Point2D.Origin, Point2D.Origin); });
+            Assert.Throws<ArgumentNullException>("dst2",
+                () => { sut.Onto(Point2D.Origin, null, Point2D.Origin); });
+            Assert.Throws<ArgumentNullException>("dst3",
+                () => { sut.Onto(Point2D.Origin, Point2D.Origin, null); });
+        }
     }
 }
-
-#endif
--- trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/Misc/LinearMapping.cs
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/Misc/LinearMapping.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnaryHeap.Utilities.D2;
+using UnaryHeap.Utilities.D3;
 
 namespace UnaryHeap.Utilities.Misc
 {
@@ -19,6 +20,23 @@ namespace UnaryHeap.Utilities.Misc
         /// <returns>A Matrix2D that will send src1 to dst1 and src2 to dst2.</returns>
         /// <exception cref="System.ArgumentNullException">dst1 or dst2 are null.</exception>
         Matrix2D Onto(Point2D dst1, Point2D dst2);
+    }
+
+    /// <summary>
+    /// Intermediate builder object for a three-dimensional linear mapping.
+    /// </summary>
+    public interface ILinearMapper3D
+    {
+        /// <summary>
+        /// Specifies the coordinates of the points to be mapped in the destination
+        /// coordinate system.
+        /// </summary>
+        /// <param name="dst1">The output coordinates of the first point.</param>
+        /// <param name="dst2">The output coordinates of the second point.</param>
+        /// <param name="dst3">The output coordinates of the third point.</param>
+        /// <returns>A Matrix2D that will send src1 to dst1, src2 to dst2 and src3 to dst3.</returns>
+        /// <exception cref="System.ArgumentNullException">dst1, dst2, or dst3 are null.</exception>
+        Matrix3D Onto(Point3D dst1, Point3D dst2, Point3D dst3);
     }
 
     /// <summary>
@@ -79,13 +97,8 @@ namespace UnaryHeap.Utilities.Misc
 
         #endregion
 
-#if INCLUDE_WORK_IN_PROGRESS
-        #region 3D Mapping
 
-        public interface ILinearMapper3D
-        {
-            Matrix3D To(Point3D dst1, Point3D dst2, Point3D dst3);
-        }
+        #region 3D Mapping
 
         class LinearMapper3D : ILinearMapper3D
         {
@@ -108,8 +121,15 @@ namespace UnaryHeap.Utilities.Misc
                 }
             }
 
-            public Matrix3D To(Point3D dst1, Point3D dst2, Point3D dst3)
+            public Matrix3D Onto(Point3D dst1, Point3D dst2, Point3D dst3)
             {
+                if (null == dst1)
+                    throw new ArgumentNullException("dst1");
+                if (null == dst2)
+                    throw new ArgumentNullException("dst2");
+                if (null == dst3)
+                    throw new ArgumentNullException("dst3");
+
                 var dest = new Matrix3D(
                     dst1.X, dst2.X, dst3.X,
                     dst1.Y, dst2.Y, dst3.Y,
@@ -119,12 +139,30 @@ namespace UnaryHeap.Utilities.Misc
             }
         }
 
+        /// <summary>
+        /// Specifies the coordinates of the points to be mapped in the source
+        /// coordinate system.
+        /// </summary>
+        /// <param name="src1">The output coordinates of the first point.</param>
+        /// <param name="src2">The output coordinates of the second point.</param>
+        /// <param name="src3">The output coordinates of the second point.</param>
+        /// <returns>An ILinearMapper3D object that is used to specify the coordinates
+        /// of the input points in the destination coordinate system..</returns>
+        /// <exception cref="System.ArgumentNullException">dst1, dst2 or dst3 are null.</exception>
+        /// <exception cref="System.ArgumentException">The input points are
+        /// linearly dependent.</exception>
         public static ILinearMapper3D From(Point3D src1, Point3D src2, Point3D src3)
         {
+            if (null == src1)
+                throw new ArgumentNullException("src1");
+            if (null == src2)
+                throw new ArgumentNullException("src2");
+            if (null == src3)
+                throw new ArgumentNullException("src3");
+
             return new LinearMapper3D(src1, src2, src3);
         }
 
         #endregion
-#endif
     }
 }
