c34be32738cc78fb47dda4b969200f446569e7af
SheepNine_cp <SND\SheepNine_cp>
2015-12-19 02:44:37 +0000

    Initial checkin of GraphPaper application

---
+++ trunk/source/UnaryHeap.Utilities/GraphPaper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GraphPaper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var viewModel = new ViewModel())
+                viewModel.Run();
+        }
+    }
+}
