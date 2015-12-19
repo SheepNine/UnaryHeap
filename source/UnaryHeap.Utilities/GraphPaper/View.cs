f122178179999d2bec0e54e47f651a4a59939a20
SheepNine_cp <sheepnine@gmail.com>
2015-12-19 16:51:45 +0000

    Hook up GraphPaper title bar to editor state

--- source/UnaryHeap.Utilities/GraphPaper/View.cs
+++ source/UnaryHeap.Utilities/GraphPaper/View.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
 
 namespace GraphPaper
 {
@@ -11,6 +14,37 @@ namespace GraphPaper
             this.viewModel = viewModel;
             InitializeComponent();
             viewModel.HookUp(wysiwygPanel1);
+            viewModel.CurrentFilenameChanged += viewModel_CurrentFilenameChanged;
+            viewModel.IsModifiedChanged += viewModel_IsModifiedChanged;
+            UpdateDialogText();
+        }
+
+        private void viewModel_IsModifiedChanged(object sender, EventArgs e)
+        {
+            UpdateDialogText();
+        }
+
+        private void viewModel_CurrentFilenameChanged(object sender, EventArgs e)
+        {
+            UpdateDialogText();
+        }
+
+        void UpdateDialogText()
+        {
+            var builder = new StringBuilder();
+
+            if (viewModel.IsModified)
+                builder.Append("*");
+
+            if (null != viewModel.CurrentFileName)
+            {
+                builder.Append(Path.GetFileNameWithoutExtension(viewModel.CurrentFileName));
+                builder.Append(" - ");
+            }
+
+            builder.Append("GraphPaper");
+
+            Text = builder.ToString();
         }
 
         private void newToolStripMenuItem_Click(object sender, System.EventArgs e)
