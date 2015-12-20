ad35d802948af94afc19099b094a42e7b1b7326d
SheepNine_cp <sheepnine@gmail.com>
2015-12-20 16:12:57 +0000

    Move classes into separate code files

---
+++ source/UnaryHeap.Utilities/GraphPaper/GraphPaperColors.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace GraphPaper
+{
+    class GraphPaperColors
+    {
+        public static Color Paper { get { return Color.FromArgb(255, 250, 240); } }
+        public static Color GridLines { get { return Color.FromArgb(100, 200, 255); } }
+        public static Color BluePen { get { return Color.FromArgb(30, 30, 160); } }
+        public static Color RedPen { get { return Color.FromArgb(220, 20, 20); } }
+    }
+}
