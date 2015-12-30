0dbec2d34bcf1f76a290da738e39b8019cd3f96c
SheepNine_cp <SND\SheepNine_cp>
2015-12-30 21:13:36 +0000

    Implement feedback display for several operations

--- trunk/source/UnaryHeap.Utilities/GraphPaper/Feedback.cs
+++ trunk/source/UnaryHeap.Utilities/GraphPaper/Feedback.cs
@@ -23,6 +23,21 @@ namespace GraphPaper
         }
     }
 
+    class UnsupportedFeedback : IFeedback
+    {
+        public bool Equals(IFeedback other)
+        {
+            var castOther = other as UnsupportedFeedback;
+            return castOther != null;
+        }
+
+        public void Render(Screen screen)
+        {
+            using (var font = new Font(FontFamily.GenericSansSerif, 16.0f))
+                screen.DrawStatusText("Unsupported Operation", font, Brushes.Red);
+        }
+    }
+
     class HoverFeedback : IFeedback
     {
         Point2D feedbackPoint;
@@ -47,8 +62,40 @@ namespace GraphPaper
             using (var pen = new Pen(GraphPaperColors.HotTrackingPen, 2.0f))
                 screen.DrawCircle(pen, feedbackPoint, 4.0f);
 
-            var display = string.Format("X: {0}\r\nY: {1}",
+            var display = string.Format("\r\nX: {0}\r\nY: {1}",
                 (double)feedbackPoint.X, (double)feedbackPoint.Y);
+
+            using (var font = new Font(FontFamily.GenericSansSerif, 16.0f))
+                screen.DrawStatusText(display, font, Brushes.Black);
+        }
+    }
+
+    class AddVertexFeedback : IFeedback
+    {
+        Point2D vertexPoint;
+
+        public AddVertexFeedback(Point2D vertexPoint)
+        {
+            this.vertexPoint = vertexPoint;
+        }
+
+        public bool Equals(IFeedback other)
+        {
+            var castOther = other as AddVertexFeedback;
+
+            if (null == castOther)
+                return false;
+
+            return this.vertexPoint.Equals(castOther.vertexPoint);
+        }
+
+        public void Render(Screen screen)
+        {
+            using (var brush = new SolidBrush(Color.CornflowerBlue))
+                screen.FillCircle(brush, vertexPoint, 5.0f);
+
+            var display = string.Format("Add Vertex\r\nX: {0}\r\nY: {1}\r\n",
+                (double)vertexPoint.X, (double)vertexPoint.Y);
 
             using (var font = new Font(FontFamily.GenericSansSerif, 16.0f))
                 screen.DrawStatusText(display, font, Brushes.Black);
@@ -87,6 +134,78 @@ namespace GraphPaper
 
             using (var pen = new Pen(Color.CornflowerBlue, 4.0f))
                 screen.DrawLine(pen, startPoint, endPoint);
+
+            var display = string.Format(
+                "Add Edge\r\nX1: {0}\r\nY1: {1}\r\nX2: {2}\r\nY2: {3}",
+                (double)startPoint.X, (double)startPoint.Y,
+                (double)endPoint.X, (double)endPoint.Y);
+
+            using (var font = new Font(FontFamily.GenericSansSerif, 16.0f))
+                screen.DrawStatusText(display, font, Brushes.Black);
+        }
+    }
+
+    class AdjustViewExtentsFeedback : IFeedback
+    {
+        Orthotope2D bounds;
+
+        public AdjustViewExtentsFeedback(Orthotope2D bounds)
+        {
+            this.bounds = bounds;
+        }
+
+        public bool Equals(IFeedback other)
+        {
+            var castObj = other as AdjustViewExtentsFeedback;
+
+            if (null == castObj)
+                return false;
+
+            return
+                this.bounds.X.Min.Equals(castObj.bounds.X.Min) &&
+                this.bounds.Y.Min.Equals(castObj.bounds.Y.Min) &&
+                this.bounds.X.Max.Equals(castObj.bounds.X.Max) &&
+                this.bounds.Y.Max.Equals(castObj.bounds.Y.Max);
+        }
+
+        public void Render(Screen screen)
+        {
+            var display = string.Format(
+                "Adjust View Extents\r\nX: {0:F2} to {1:F2}\r\nY: {2:F2} to {3:F2}",
+                (double)bounds.X.Min, (double)bounds.X.Max,
+                (double)bounds.Y.Min, (double)bounds.Y.Max);
+
+            using (var font = new Font(FontFamily.GenericSansSerif, 16.0f))
+                screen.DrawStatusText(display, font, Brushes.Black);
+        }
+    }
+
+    class CenterViewFeedback : IFeedback
+    {
+        Point2D newCenterPoint;
+
+        public CenterViewFeedback(Point2D newCenterPoint)
+        {
+            this.newCenterPoint = newCenterPoint;
+        }
+
+        public bool Equals(IFeedback other)
+        {
+            var castObj = other as CenterViewFeedback;
+
+            if (null == castObj)
+                return false;
+
+            return this.newCenterPoint.Equals(castObj.newCenterPoint);
+        }
+
+        public void Render(Screen screen)
+        {
+            var display = string.Format("Center View\r\nX: {0:F2}\r\nY: {1:F2}\r\n",
+                (double)newCenterPoint.X, (double)newCenterPoint.Y);
+
+            using (var font = new Font(FontFamily.GenericSansSerif, 16.0f))
+                screen.DrawStatusText(display, font, Brushes.Black);
         }
     }
 }
