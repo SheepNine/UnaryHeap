7665a9f529c91806ca448665cbc22463c4518917
SheepNine_cp <sheepnine@gmail.com>
2016-01-01 19:31:24 +0000

    Flesh out metadata edit procedure

---
+++ source/UnaryHeap.Utilities/GraphPaper/Metadata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaryHeap.Utilities.D2;
+
+namespace GraphPaper
+{
+    class Metadata
+    {
+        public static MetadataChange ViewAndEdit(MetadataSet input)
+        {
+            var output = ShowUIForEdit(input);
+
+            if (null == output)
+                return null;
+
+            return input.GetChangeTo(output);
+        }
+
+        static MetadataSet ShowUIForEdit(MetadataSet input)
+        {
+            return null;
+        }
+    }
+
+    class MetadataSet
+    {
+        public MetadataChange GetChangeTo(MetadataSet output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class MetadataChange
+    {
+        internal void UpdateGraphMetadata(Graph2D graph)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UpdateVertexMetadata(Graph2D graph, GraphObjectSelection selection)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UpdateEdgeMetadata(Graph2D graph, GraphObjectSelection selection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
