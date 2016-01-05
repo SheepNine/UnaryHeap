f49c19d183343cfafd60a6fe2e43c44ce5ce3cf0
SheepNine_cp <SND\SheepNine_cp>
2016-01-05 02:48:39 +0000

    Implement first cut of partitioning algorithm

---
+++ trunk/source/UnaryHeap.Utilities/Partitioner/BspNode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaryHeap.Utilities.D2;
+
+namespace Partitioner
+{
+    class BspNode
+    {
+        public Hyperplane2D splitter;
+        public BspNode frontChild;
+        public BspNode backChild;
+        public List<Surface> surfaces;
+
+        private BspNode() { }
+
+        public static BspNode LeafNode(IEnumerable<Surface> surfaces)
+        {
+            return new BspNode()
+            {
+                splitter = null,
+                frontChild = null,
+                backChild = null,
+                surfaces = surfaces.ToList()
+            };
+        }
+
+        public static BspNode BranchNode(Hyperplane2D splitter,
+            BspNode frontChild, BspNode backChild)
+        {
+            return new BspNode()
+            {
+                splitter = splitter,
+                frontChild = frontChild,
+                backChild = backChild,
+                surfaces = null
+            };
+        }
+
+        public bool IsLeaf
+        {
+            get { return surfaces != null; }
+        }
+
+        public Hyperplane2D Splitter
+        {
+            get
+            {
+                if (IsLeaf)
+                    throw new InvalidOperationException("Leaf nodes have no splitter.");
+
+                return splitter;
+            }
+        }
+
+        public BspNode FrontChild
+        {
+            get
+            {
+                if (IsLeaf)
+                    throw new InvalidOperationException("Leaf nodes have no children.");
+
+                return frontChild;
+            }
+        }
+
+        public BspNode BackChild
+        {
+            get
+            {
+                if (IsLeaf)
+                    throw new InvalidOperationException("Leaf nodes have no children.");
+
+                return backChild;
+            }
+        }
+
+        public IEnumerable<Surface> Surfaces
+        {
+            get
+            {
+                if (false == IsLeaf)
+                    throw new InvalidOperationException("Branch nodes have no surfaces.");
+
+                return surfaces;
+            }
+        }
+    }
+}
