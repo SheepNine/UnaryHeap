9b4957bfa88c428e11cfbcff923377849714f691
SheepNine_cp <SND\SheepNine_cp>
2016-04-14 02:10:45 +0000

    Add reference to NUnit 3.2 and migrate first test fixture to it

--- trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/AffineMapping1DTests.cs
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities.Tests/AffineMapping1DTests.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using UnaryHeap.Utilities.Core;
 using UnaryHeap.Utilities.Misc;
-using Xunit;
+using NUnit.Framework;
 
 namespace UnaryHeap.Utilities.Tests
 {
+    [TestFixture]
     public class AffineMapping1DTests
     {
-        [Fact]
+        [Test]
         public void NonSingularResult()
         {
             Rational src1 = 2;
@@ -18,19 +19,19 @@ namespace UnaryHeap.Utilities.Tests
             // The order that the points are specified should not affect the results
             var sut1 = AffineMapping.From(src1, src2).Onto(dst1, dst2);
             var sut2 = AffineMapping.From(src2, src1).Onto(dst2, dst1);
-
-            Assert.Equal(dst1, (sut1 * src1.Homogenized()).Dehomogenized());
-            Assert.Equal(dst2, (sut1 * src2.Homogenized()).Dehomogenized());
-            Assert.Equal(dst1, (sut2 * src1.Homogenized()).Dehomogenized());
-            Assert.Equal(dst2, (sut2 * src2.Homogenized()).Dehomogenized());
+            
+            Assert.AreEqual(dst1, (sut1 * src1.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst2, (sut1 * src2.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst1, (sut2 * src1.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst2, (sut2 * src2.Homogenized()).Dehomogenized());
 
             var sutInv = sut1.ComputeInverse();
 
-            Assert.Equal(src1, (sutInv * dst1.Homogenized()).Dehomogenized());
-            Assert.Equal(src2, (sutInv * dst2.Homogenized()).Dehomogenized());
+            Assert.AreEqual(src1, (sutInv * dst1.Homogenized()).Dehomogenized());
+            Assert.AreEqual(src2, (sutInv * dst2.Homogenized()).Dehomogenized());
         }
 
-        [Fact]
+        [Test]
         public void SingularResult()
         {
             Rational src1 = 2;
@@ -42,30 +43,26 @@ namespace UnaryHeap.Utilities.Tests
             var sut1 = AffineMapping.From(src1, src2).Onto(dst1, dst2);
             var sut2 = AffineMapping.From(src2, src1).Onto(dst2, dst1);
 
-            Assert.Equal(dst1, (sut1 * src1.Homogenized()).Dehomogenized());
-            Assert.Equal(dst2, (sut1 * src2.Homogenized()).Dehomogenized());
-            Assert.Equal(dst1, (sut2 * src1.Homogenized()).Dehomogenized());
-            Assert.Equal(dst2, (sut2 * src2.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst1, (sut1 * src1.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst2, (sut1 * src2.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst1, (sut2 * src1.Homogenized()).Dehomogenized());
+            Assert.AreEqual(dst2, (sut2 * src2.Homogenized()).Dehomogenized());
 
             Assert.Throws<InvalidOperationException>(() => { sut1.ComputeInverse(); });
         }
 
-        [Fact]
+        [Test]
         public void SimpleArgumentExceptions()
         {
-            Assert.Throws<ArgumentNullException>("src1",
-                () => { AffineMapping.From(null, 0); });
-            Assert.Throws<ArgumentNullException>("src2",
-                () => { AffineMapping.From(0, null); });
+            Assert.Throws<ArgumentNullException>(() => { AffineMapping.From(null, 0); });
+            Assert.Throws<ArgumentNullException>(() => { AffineMapping.From(0, null); });
 
             var from = AffineMapping.From(0, 1);
 
-            Assert.Throws<ArgumentNullException>("dst1",
-                () => { from.Onto(null, 0); });
-            Assert.Throws<ArgumentNullException>("dst2",
-                () => { from.Onto(0, null); });
+            Assert.Throws<ArgumentNullException>(() => { from.Onto(null, 0); });
+            Assert.Throws<ArgumentNullException>(() => { from.Onto(0, null); });
 
-            Assert.Equal("Source points are linearly dependent; cannot invert.",
+            Assert.AreEqual("Source points are linearly dependent; cannot invert.",
                 Assert.Throws<ArgumentException>(
                 () => { AffineMapping.From(1, 1); })
                 .Message);
