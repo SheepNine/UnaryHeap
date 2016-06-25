6fe68808ec877fd8ca0fce0965eecf7a5a0b152e
SheepNine_cp <SND\SheepNine_cp>
2016-06-25 03:13:49 +0000

    Check in work-in-progress of Retrographic data model

---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/Retrographic/Color.cs
@@ -0,0 +1,75 @@
+﻿#if INCLUDE_WORK_IN_PROGRESS
+
+using System;
+using System.IO;
+
+namespace UnaryHeap.Utilities.Retrographic
+{
+    class Color
+    {
+        // |               1               |               0               |
+        // |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // |---|-------------------|-------------------|-------------------|
+        // | T |        RED        |       GREEN       |       BLUE        |
+        // |---|-----------^-------|-------^-----------|---^---------------|
+        const int TransparentMask = 0x8000;
+        const int RedMask = 0x7C00;
+        const int GreenMask = 0x03E0;
+        const int BlueMask = 0x001F;
+
+        const int UnusedColorBits = 0x07;
+
+        public bool Transparent { get; private set; }
+        public byte R { get; private set; }
+        public byte G { get; private set; }
+        public byte B { get; private set; }
+
+        public Color(bool transparent, byte r, byte g, byte b)
+        {
+            if ((r & UnusedColorBits) != 0)
+                throw new ArgumentOutOfRangeException("r");
+            if ((g & UnusedColorBits) != 0)
+                throw new ArgumentOutOfRangeException("g");
+            if ((b & UnusedColorBits) != 0)
+                throw new ArgumentOutOfRangeException("b");
+
+            Transparent = transparent;
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public void Serialize(Stream output)
+        {
+            int encodedBytes =
+                (Transparent ? TransparentMask : 0) |
+                (R << 7) |
+                (G << 2) |
+                (B >> 3);
+
+            output.WriteByte((byte)(encodedBytes));
+            output.WriteByte((byte)(encodedBytes >> 8));
+        }
+
+        public static Color Deserialize(Stream input)
+        {
+            int byte1 = input.ReadByte();
+            if (byte1 == -1)
+                throw new InvalidDataException("End of stream reached");
+            int byte2 = input.ReadByte();
+            if (byte2 == -1)
+                throw new InvalidDataException("End of stream reached");
+
+            int encodedBytes = (byte2 << 8) | byte1;
+
+            return new Color(
+                (encodedBytes & TransparentMask) == TransparentMask,
+                (byte)((encodedBytes & RedMask) >> 7),
+                (byte)((encodedBytes & GreenMask) >> 2),
+                (byte)((encodedBytes & BlueMask) << 3)
+            );
+        }
+    }
+}
+
+#endif
---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/Retrographic/Mapping.cs
@@ -0,0 +1,87 @@
+﻿#if INCLUDE_WORK_IN_PROGRESS
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaryHeap.Utilities.Retrographic
+{
+    class Mapping
+    {
+        // |               1               |               0               |
+        // |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // |-------------------------------|-------|---|---|---|-----------|
+        // |              TILE             |  PAGE |ITX|ITY| M |  PALETTE  |
+        // |---------------^---------------|-------|---|---|---|-----------|
+        const int TileMask = 0xFF00;
+        const int PageMask = 0x00C0;
+        const int InvertTileXMask = 0x0020;
+        const int InvertTileYMask = 0x0010;
+        const int MaskedMask = 0x0008;
+        const int PaletteMask = 0x0007;
+
+        public int Tile { get; private set; }
+        public int Page { get; private set; }
+        public bool InvertTileX { get; private set; }
+        public bool InvertTileY { get; private set; }
+        public bool Masked { get; private set; }
+        public int Palette { get; private set; }
+
+        public Mapping(int tile, int page, bool invertTileX, bool invertTileY,
+            bool masked, int palette)
+        {
+            if (tile < 0 || tile >= 256)
+                throw new ArgumentOutOfRangeException("tile");
+            if (page < 0 || page >= 4)
+                throw new ArgumentOutOfRangeException("page");
+            if (palette < 0 || palette >= 8)
+                throw new ArgumentOutOfRangeException("palette");
+
+            Tile = tile;
+            Page = page;
+            InvertTileX = invertTileX;
+            InvertTileY = invertTileY;
+            Masked = masked;
+            Palette = palette;
+        }
+
+        public void Serialize(Stream output)
+        {
+            int encodedBytes =
+                (Tile << 8) |
+                (Page << 6) |
+                (InvertTileX ? InvertTileXMask : 0) |
+                (InvertTileY ? InvertTileYMask : 0) |
+                (Masked ? MaskedMask : 0) |
+                (Palette);
+
+            output.WriteByte((byte)(encodedBytes));
+            output.WriteByte((byte)(encodedBytes >> 8));
+        }
+
+        public static Mapping Deserialize(Stream input)
+        {
+            int byte1 = input.ReadByte();
+            if (byte1 == -1)
+                throw new InvalidDataException("End of stream reached");
+            int byte2 = input.ReadByte();
+            if (byte2 == -1)
+                throw new InvalidDataException("End of stream reached");
+
+            int encodedBytes = (byte2 << 8) | byte1;
+            return new Mapping(
+                (encodedBytes & TileMask) >> 8,
+                (encodedBytes & PageMask) >> 6,
+                (encodedBytes & InvertTileXMask) == InvertTileXMask,
+                (encodedBytes & InvertTileYMask) == InvertTileYMask,
+                (encodedBytes & MaskedMask) == MaskedMask,
+                (encodedBytes & PaletteMask)
+            );
+        }
+    }
+}
+
+#endif
