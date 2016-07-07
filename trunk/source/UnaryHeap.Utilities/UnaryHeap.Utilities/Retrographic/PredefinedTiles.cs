95f714f12318ad82435d70d470df22a0c8ab3bde
SheepNine_cp <SND\SheepNine_cp>
2016-07-07 00:33:32 +0000

    Add utility methods to create hardcoded tiles

---
+++ trunk/source/UnaryHeap.Utilities/UnaryHeap.Utilities/Retrographic/PredefinedTiles.cs
@@ -0,0 +1,90 @@
+﻿#if INCLUDE_WORK_IN_PROGRESS_RETROGRAPHIC
+
+namespace UnaryHeap.Utilities.Retrographic
+{
+    static class PredefinedTiles
+    {
+        public static Tile Char_A { get { return new Tile(CHAR_A); } }
+        static string CHAR_A =
+            "01111100" +
+            "11000110" +
+            "11000110" +
+            "11111110" +
+            "11000110" +
+            "11000110" +
+            "11000110" +
+            "22222223";
+        public static Tile Char_B { get { return new Tile(CHAR_B); } }
+        static string CHAR_B =
+            "11111100" +
+            "11000110" +
+            "11000110" +
+            "11111110" +
+            "11000110" +
+            "11000110" +
+            "11111100" +
+            "22222223";
+        public static Tile Char_C { get { return new Tile(CHAR_C); } }
+        static string CHAR_C =
+            "01111110" +
+            "11000000" +
+            "11000000" +
+            "11000000" +
+            "11000000" +
+            "11000000" +
+            "01111110" +
+            "22222223";
+        public static Tile Char_D { get { return new Tile(CHAR_D); } }
+        static string CHAR_D =
+            "11111100" +
+            "11000110" +
+            "11000110" +
+            "11000110" +
+            "11000110" +
+            "11000110" +
+            "11111100" +
+            "22222223";
+        public static Tile Char_E { get { return new Tile(CHAR_E); } }
+        static string CHAR_E =
+            "11111110" +
+            "11000000" +
+            "11000000" +
+            "11111110" +
+            "11000000" +
+            "11000000" +
+            "11111110" +
+            "22222223";
+        public static Tile Char_F { get { return new Tile(CHAR_F); } }
+        static string CHAR_F =
+            "11111110" +
+            "11000000" +
+            "11000000" +
+            "11111110" +
+            "11000000" +
+            "11000000" +
+            "11000000" +
+            "22222223";
+        public static Tile Char_G { get { return new Tile(CHAR_G); } }
+        static string CHAR_G =
+            "01111110" +
+            "11000000" +
+            "11000000" +
+            "11001110" +
+            "11000110" +
+            "11000110" +
+            "01111110" +
+            "22222223";
+        public static Tile Char_H { get { return new Tile(CHAR_H); } }
+        static string CHAR_H =
+            "11000110" +
+            "11000110" +
+            "11000110" +
+            "11111110" +
+            "11000110" +
+            "11000110" +
+            "11000110" +
+            "22222223";
+    }
+}
+
+#endif
