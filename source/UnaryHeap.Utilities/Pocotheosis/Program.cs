2653936d1e5076c7f8075cded604bf6d8bc7a5b1
SheepNine <sheepnine@gmail.com>
2016-09-04 11:02:44 -0400

    Generate hardcoded sample output

--- source/UnaryHeap.Utilities/Pocotheosis/Program.cs
+++ source/UnaryHeap.Utilities/Pocotheosis/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Pocotheosis
+﻿using System.IO;
+
+namespace Pocotheosis
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("HELLO WORLD");
+            File.WriteAllText("Pocos.cs", @"namespace NAMESPAAAACE
+{
+    public class POCO1
+    {
+        public global::System.Int32 VAR1 { get; private set; }
+        public global::System.Int64 VAR2 { get; private set; }
+
+        public POCO1(global::System.Int32 VAR1, global::System.Int64 VAR2)
+        {
+            this.VAR1 = VAR1;
+            this.VAR2 = VAR2;
+        }
+    }
+}");
         }
     }
 }
