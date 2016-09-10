cfef628f3dbd6b107c61b4efa6f17ba0eb64c633
SheepNine <sheepnine@gmail.com>
2016-09-10 15:03:45 -0400

    Create Reversi project and add game logic class

---
+++ source/UnaryHeap.Utilities/Reversi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
