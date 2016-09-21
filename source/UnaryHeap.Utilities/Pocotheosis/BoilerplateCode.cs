375c29e71ed9e28c9660ba9a5339437b28e3edaf
SheepNine <sheepnine@gmail.com>
2016-09-20 21:34:14 -0400

    Move server hosting functionality into generated code files

--- source/UnaryHeap.Utilities/Pocotheosis/BoilerplateCode.cs
+++ source/UnaryHeap.Utilities/Pocotheosis/BoilerplateCode.cs
@@ -460,6 +460,9 @@ namespace Pocotheosis
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading;
 
     class ConnectionAdded : Poco
     {
@@ -588,6 +591,104 @@ namespace Pocotheosis
                     connection.Value.Close();
                 isOpen = false;
             }
+        }
+    }
+
+
+    public interface IServer
+    {
+        void Start();
+        void RequestShutdown();
+    }
+
+    public interface IServerLogicFactory
+    {
+        IServerLogic Create(IServerLogicCallbacks callbacks);
+    }
+
+    public interface IServerLogic
+    {
+        void Process(Guid sender, Poco poco);
+        void Shutdown();
+    }
+
+    public interface IServerLogicCallbacks
+    {
+        void Send(Poco poco, params Guid[] recipients);
+        void RequestDisconnect(Guid connectionId);
+        void RequestShutdown();
+    }
+
+
+    class Server : IServerLogicCallbacks, IServer
+    {
+        public static IServer Create(IPAddress address, int port, IServerLogicFactory factory)
+        {
+            return new Server(address, port, factory);
+        }
+
+        PocoServerEndpoint endpoint;
+        IServerLogic logic;
+        TcpListener listener;
+
+        private Server(IPAddress address, int port, IServerLogicFactory factory)
+        {
+            endpoint = new PocoServerEndpoint();
+            listener = new TcpListener(address, port);
+            logic = factory.Create(this);
+        }
+
+        private void BeginAcceptTcpClientCallback(IAsyncResult asyncResult)
+        {
+            try
+            {
+                TcpClient client = listener.EndAcceptTcpClient(asyncResult);
+                endpoint.AddConnection(Guid.NewGuid(), client.GetStream());
+                listener.BeginAcceptTcpClient(BeginAcceptTcpClientCallback, null);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            listener.BeginAcceptTcpClient(BeginAcceptTcpClientCallback, null);
+            new Thread(ServerThreadMain) { IsBackground = true }.Start();
+        }
+
+        void ServerThreadMain()
+        {
+            while (true)
+            {
+                var nextMessage = endpoint.Receive();
+                if (nextMessage.Item2 != null)
+                {
+                    logic.Process(nextMessage.Item1, nextMessage.Item2);
+                }
+                else
+                {
+                    logic.Shutdown();
+                    return;
+                }
+            }
+        }
+
+        public void RequestDisconnect(Guid connectionId)
+        {
+            endpoint.Disconnect(connectionId);
+        }
+
+        public void RequestShutdown()
+        {
+            listener.Stop();
+            endpoint.Close();
+        }
+
+        public void Send(Poco poco, params Guid[] recipients)
+        {
+            endpoint.Send(poco, recipients);
         }
     }");
         }
