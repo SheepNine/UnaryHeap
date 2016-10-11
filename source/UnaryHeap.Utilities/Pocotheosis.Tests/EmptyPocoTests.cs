275be268d9af270c964a2021e141801e79ae75d7
SheepNine <sheepnine@gmail.com>
2016-10-11 19:26:15 -0400

    Add Pocotheosis.Tests project

---
+++ source/UnaryHeap.Utilities/Pocotheosis.Tests/EmptyPocoTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Pocotheosis.Tests.Pocos;
+
+namespace Pocotheosis.Tests
+{
+    [TestFixture]
+    public class EmptyPocoTests
+    {
+        [Test]
+        public void Equality()
+        {
+            Assert.AreNotEqual(null, new EmptyPoco());
+            Assert.AreEqual(new EmptyPoco(), new EmptyPoco());
+        }
+    }
+}
