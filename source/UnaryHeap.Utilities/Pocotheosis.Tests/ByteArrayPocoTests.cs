4014b8c2ea90aea42f6120143dddec3ab0d27ab6
SheepNine <sheepnine@gmail.com>
2016-10-11 20:42:13 -0400

    Add (failing) test cases for byte array poco

---
+++ source/UnaryHeap.Utilities/Pocotheosis.Tests/ByteArrayPocoTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Pocotheosis.Tests.Pocos;
+
+namespace Pocotheosis.Tests
+{
+    [TestFixture]
+    public class ByteArrayPocoTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            Assert.AreEqual(0, new ByteArrayPoco(new byte[0]).Orrey.Count);
+            var data = new byte[] { 17, 88 };
+            var poco = new ByteArrayPoco(data);
+            Assert.AreEqual(2, poco.Orrey.Count);
+            data[0] = 0; // Ensures poco made a copy
+            Assert.AreEqual(17, poco.Orrey[0]);
+            Assert.AreEqual(88, poco.Orrey[1]);
+            Assert.True(poco.Orrey.IsReadOnly);
+        }
+
+        [Test]
+        [Ignore("TODO: implement this")]
+        public void Equality()
+        {
+            Assert.AreNotEqual(null, new ByteArrayPoco(new byte[] { 1, 3 }));
+            Assert.AreEqual(new ByteArrayPoco(new byte[] { 1, 3 }), new ByteArrayPoco(new byte[] { 1, 3 }));
+            Assert.AreNotEqual(new ByteArrayPoco(new byte[] { 1, 6 }), new ByteArrayPoco(new byte[] { 1, 3 }));
+            Assert.AreNotEqual(new ByteArrayPoco(new byte[] { 1, 3, 4 }), new ByteArrayPoco(new byte[] { 1, 3 }));
+            Assert.AreNotEqual(new ByteArrayPoco(new byte[] { 9 }), new ByteArrayPoco(new byte[] { 1, 3 }));
+        }
+
+        [Test]
+        [Ignore("TODO: implement this")]
+        public void StringFormat()
+        {
+            Assert.AreEqual("ByteArrayPoco\r\n\tOrrey: <empty>", new ByteArrayPoco(new byte[] { }).ToString());
+            Assert.AreEqual("ByteArrayPoco\r\n\tOrrey: 44", new ByteArrayPoco(new byte[] { 44 }).ToString());
+            Assert.AreEqual("ByteArrayPoco\r\n\tOrrey: 44, 88", new ByteArrayPoco(new byte[] { 44, 88 }).ToString());
+        }
+
+        [Test]
+        [Ignore("TODO: implement this")]
+        public void RoundTrip()
+        {
+            TestUtils.TestRoundTrip(new ByteArrayPoco(new byte[] { }));
+            TestUtils.TestRoundTrip(new ByteArrayPoco(new byte[] { 44 }));
+            TestUtils.TestRoundTrip(new ByteArrayPoco(new byte[] { 44, 88 }));
+        }
+    }
+}
