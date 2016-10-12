97595dc8af2ffa174bf81b1e96ae09c63c86ac8d
SheepNine <sheepnine@gmail.com>
2016-10-11 20:16:15 -0400

    Add tests for byte and string poco types

---
+++ source/UnaryHeap.Utilities/Pocotheosis.Tests/BytePocoTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Pocotheosis.Tests.Pocos;
+
+namespace Pocotheosis.Tests
+{
+    [TestFixture]
+    public class BytePocoTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            Assert.AreEqual(byte.MinValue, new BytePoco(byte.MinValue).Cheese);
+            Assert.AreEqual(byte.MaxValue, new BytePoco(byte.MaxValue).Cheese);
+        }
+
+        [Test]
+        public void Equality()
+        {
+            Assert.AreNotEqual(null, new BytePoco(15));
+            Assert.AreEqual(new BytePoco(15), new BytePoco(15));
+            Assert.AreNotEqual(new BytePoco(30), new BytePoco(15));
+        }
+
+        [Test]
+        public void StringFormat()
+        {
+            Assert.AreEqual("BytePoco\r\n\tCheese: 19", new BytePoco(19).ToString());
+            Assert.AreEqual("BytePoco\r\n\tCheese: 44", new BytePoco(44).ToString());
+        }
+
+        [Test]
+        public void RoundTrip()
+        {
+            TestUtils.TestRoundTrip(new BytePoco(byte.MinValue));
+            TestUtils.TestRoundTrip(new BytePoco(42));
+            TestUtils.TestRoundTrip(new BytePoco(byte.MaxValue));
+        }
+    }
+}
---
+++ source/UnaryHeap.Utilities/Pocotheosis.Tests/StringPocoTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Pocotheosis.Tests.Pocos;
+
+namespace Pocotheosis.Tests
+{
+    [TestFixture]
+    public class StringPocoTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            Assert.AreEqual(string.Empty, new StringPoco(string.Empty).Twine);
+            Assert.AreEqual("12 Foot", new StringPoco("12 Foot").Twine);
+        }
+
+        [Test]
+        [Ignore("TODO: Make this test pass with appropriate check")]
+        public void Constructor_NullReference()
+        {
+            Assert.Throws<System.ArgumentNullException>(() => new StringPoco(null));
+        }
+
+        [Test]
+        public void Equality()
+        {
+            Assert.AreNotEqual(null, new StringPoco("Alpha"));
+            Assert.AreEqual(new StringPoco("Alpha"), new StringPoco("Alpha"));
+            Assert.AreNotEqual(new StringPoco("Beta"), new StringPoco("Alpha"));
+        }
+
+        [Test]
+        public void StringFormat()
+        {
+            Assert.AreEqual("StringPoco\r\n\tTwine: Fortune", new StringPoco("Fortune").ToString());
+            Assert.AreEqual("StringPoco\r\n\tTwine: A value\r\nwith newlines", new StringPoco("A value\r\nwith newlines").ToString());
+        }
+
+        [Test]
+        public void RoundTrip()
+        {
+            TestUtils.TestRoundTrip(new StringPoco(string.Empty));
+            TestUtils.TestRoundTrip(new StringPoco("string.NotEmpty"));
+        }
+    }
+}
