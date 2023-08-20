aacb811def66f83563e650054ab5e15f6c3c6e8c
SheepNine <sheepnine@gmail.com>
2023-08-20 09:45:49 -0400

    Class map tests

---
+++ source/Pocotheosis/Pocotheosis.Tests/Maps/ClassMapTests.cs
@@ -0,0 +1,162 @@
+﻿using NUnit.Framework;
+using Pocotheosis.Tests.Pocos;
+using System;
+using Dataset = System.Collections.Generic.Dictionary
+    <string, Pocotheosis.Tests.Pocos.PrimitiveValue>;
+
+namespace Pocotheosis.Tests.Maps
+{
+    [TestFixture]
+    public class ClassMapTests
+    {
+        static PrimitiveValue P(byte value) { return new PrimitiveValue(value); }
+
+        [Test]
+        public void Constructor()
+        {
+            var data = new Dataset()
+            {
+                { "3", P(1) },
+                { "5", P(0) }
+            };
+            var sut = new ClassMap(data);
+            data.Clear(); // Ensures that sut made a copy
+
+            Assert.AreEqual(2, sut.Pocos.Count);
+            Assert.AreEqual(1, sut.Pocos["3"].Primitive);
+            Assert.AreEqual(0, sut.Pocos["5"].Primitive);
+
+            sut = new ClassMap(new Dataset());
+            Assert.AreEqual(0, sut.Pocos.Count);
+        }
+
+        [Test]
+        public void ConstructorNullReference()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new ClassMap(null));
+            Assert.Throws<ArgumentNullException>(
+                () => new ClassMap(new Dataset() { { "1", null } }));
+        }
+
+        [Test]
+        public void Equality()
+        {
+            var datasets = new[]
+            {
+                new Dataset(),
+                new Dataset() { { "1", P(0) } },
+                new Dataset() { { "2", P(0) } },
+                new Dataset() { { "1", P(1) } },
+                new Dataset() { { "1", P(0) }, { "2", P(0) } },
+            };
+
+            for (int i = 0; i < datasets.Length; i++)
+                for (int j = 0; j < datasets.Length; j++)
+                    Assert.AreEqual(i == j, new ClassMap(datasets[i])
+                        .Equals(new ClassMap(datasets[j])));
+        }
+
+        [Test]
+        public void Builder()
+        {
+            var source = new ClassMap(new Dataset() {
+                { "5", P(1) }
+            });
+
+            var target = new ClassMap(new Dataset() {
+                { "3", P(1) },
+                { "7", P(0) }
+            });
+
+            {
+                var sut = source.ToBuilder();
+                sut.RemovePoco("5");
+                sut.SetPoco("7", P(0));
+                sut.SetPoco("3", P(1));
+                Assert.AreEqual(target, sut.Build());
+
+                Assert.AreEqual(2, sut.CountPocos);
+                Assert.AreEqual(new[] { "3", "7" }, sut.PocoKeys);
+            }
+
+            {
+                var sut = target.ToBuilder();
+                sut.ClearPocos();
+                sut.SetPoco("5", P(1));
+                Assert.AreEqual(source, sut.Build());
+
+                Assert.AreEqual(1, sut.CountPocos);
+                Assert.AreEqual(new[] { "5" }, sut.PocoKeys);
+            }
+        }
+
+        [Test]
+        public void Checksum()
+        {
+            PocoTest.Checksum(
+                new ClassMap(new Dataset() {
+                    { "3", P(0) }
+                }),
+                "ba390572f657a894441aee73d4063315aa8dd89462d75123b077ebb409ab3cbf");
+        }
+
+        [Test]
+        public void StringFormat()
+        {
+            PocoTest.StringFormat(new() { {
+                new ClassMap(new Dataset()
+                {
+                    { "3", P(1) },
+                    { "5", P(0) }
+                }),
+                @"{
+                    Pocos = (
+                        '3' -> {
+                            Primitive = 1
+                        },
+                        '5' -> {
+                            Primitive = 0
+                        }
+                    )
+                }"
+            } });
+        }
+
+        [Test]
+        public void Serialization()
+        {
+            PocoTest.Serialization(
+                new ClassMap(new Dataset()
+                {
+                    { "-99", P(0) },
+                    { "7", P(1) }
+                }),
+                new ClassMap(new Dataset())
+            );
+        }
+
+        [Test]
+        public void JsonSerialization()
+        {
+            PocoTest.JsonSerialization<ClassMap>(@"{
+                ""Pocos"": {}
+            }", @"{
+                ""Pocos"": {
+                    ""3"": {
+                        ""Primitive"": 0
+                    }
+                }
+            }", @"{
+                ""Pocos"": {
+                    ""3"": {
+                        ""Primitive"": 1
+                    },
+                    ""5"": {
+                        ""Primitive"": 0
+                    }
+                }
+            }");
+        }
+    }
+}
---
+++ source/Pocotheosis/Pocotheosis.Tests/Maps/NullableClassMapTests.cs
@@ -0,0 +1,170 @@
+﻿using NUnit.Framework;
+using Pocotheosis.Tests.Pocos;
+using System;
+using Dataset = System.Collections.Generic.Dictionary
+    <string, Pocotheosis.Tests.Pocos.PrimitiveValue>;
+
+namespace Pocotheosis.Tests.Maps
+{
+    [TestFixture]
+    public class NullableClassMapTests
+    {
+        static PrimitiveValue P(byte value) { return new PrimitiveValue(value); }
+
+        [Test]
+        public void Constructor()
+        {
+            var data = new Dataset()
+            {
+                { "3", P(1) },
+                { "7", null },
+                { "5", P(0) }
+            };
+            var sut = new NullableClassMap(data);
+            data.Clear(); // Ensures that sut made a copy
+
+            Assert.AreEqual(3, sut.MaybePocos.Count);
+            Assert.AreEqual(1, sut.MaybePocos["3"].Primitive);
+            Assert.IsNull(sut.MaybePocos["7"]);
+            Assert.AreEqual(0, sut.MaybePocos["5"].Primitive);
+
+            sut = new NullableClassMap(new Dataset());
+            Assert.AreEqual(0, sut.MaybePocos.Count);
+        }
+
+        [Test]
+        public void ConstructorNullReference()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new NullableClassMap(null));
+        }
+
+        [Test]
+        public void Equality()
+        {
+            var datasets = new[]
+            {
+                new Dataset(),
+                new Dataset() { { "1", P(0) } },
+                new Dataset() { { "2", P(0) } },
+                new Dataset() { { "1", P(1) } },
+                new Dataset() { { "1", null } },
+                new Dataset() { { "1", P(0) }, { "2", P(0) } },
+            };
+
+            for (int i = 0; i < datasets.Length; i++)
+                for (int j = 0; j < datasets.Length; j++)
+                    Assert.AreEqual(i == j, new NullableClassMap(datasets[i])
+                        .Equals(new NullableClassMap(datasets[j])));
+        }
+
+        [Test]
+        public void Builder()
+        {
+            var source = new NullableClassMap(new Dataset() {
+                { "3", null },
+                { "5", P(1) }
+            });
+
+            var target = new NullableClassMap(new Dataset() {
+                { "3", P(1) },
+                { "7", P(0) }
+            });
+
+            {
+                var sut = source.ToBuilder();
+                sut.SetMaybePoco("3", P(1));
+                sut.RemoveMaybePoco("5");
+                sut.SetMaybePoco("7", P(0));
+                Assert.AreEqual(target, sut.Build());
+
+                Assert.AreEqual(2, sut.CountMaybePocos);
+                Assert.AreEqual(new[] { "3", "7" }, sut.MaybePocoKeys);
+            }
+
+            {
+                var sut = target.ToBuilder();
+                sut.ClearMaybePocos();
+                sut.SetMaybePoco("3", null);
+                sut.SetMaybePoco("5", P(1));
+                Assert.AreEqual(source, sut.Build());
+
+                Assert.AreEqual(2, sut.CountMaybePocos);
+                Assert.AreEqual(new[] { "3", "5" }, sut.MaybePocoKeys);
+            }
+        }
+
+        [Test]
+        public void Checksum()
+        {
+            PocoTest.Checksum(
+                new NullableClassMap(new Dataset() {
+                    { "3", P(0) },
+                    { "5", null }
+                }),
+                "9a8238c467801aa1e4a5b71cb568a9f833fb1c38badc95c746e860326f014db8");
+        }
+
+        [Test]
+        public void StringFormat()
+        {
+            PocoTest.StringFormat(new() { {
+                new NullableClassMap(new Dataset()
+                {
+                    { "3", P(1) },
+                    { "5", P(0) },
+                    { "4", null }
+                }),
+                @"{
+                    MaybePocos = (
+                        '3' -> {
+                            Primitive = 1
+                        },
+                        '4' -> null,
+                        '5' -> {
+                            Primitive = 0
+                        }
+                    )
+                }"
+            } });
+        }
+
+        [Test]
+        public void Serialization()
+        {
+            PocoTest.Serialization(
+                new NullableClassMap(new Dataset()
+                {
+                    { "-99", P(0) },
+                    { "7", P(1) },
+                    { "3", null },
+                }),
+                new NullableClassMap(new Dataset())
+            );
+        }
+
+        [Test]
+        public void JsonSerialization()
+        {
+            PocoTest.JsonSerialization<NullableClassMap>(@"{
+                ""MaybePocos"": {}
+            }", @"{
+                ""MaybePocos"": {
+                    ""3"": {
+                        ""Primitive"": 0
+                    }
+                }
+            }", @"{
+                ""MaybePocos"": {
+                    ""3"": {
+                        ""Primitive"": 1
+                    },
+                    ""4"": null,
+                    ""5"": {
+                        ""Primitive"": 0
+                    }
+                }
+            }");
+        }
+    }
+}
