b9f5f3d8fc4edbc011a3fd5f3fea9962c2da68e6
SheepNine <sheepnine@gmail.com>
2023-09-11 13:03:03 -0400

    Add (failing) tests for PolymorphicValue poco

---
+++ source/Pocotheosis/Pocotheosis.Tests/Values/PolymorphicValueTests.cs
@@ -0,0 +1,57 @@
+﻿using GeneratedTestPocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocotheosis.Tests.Values
+{
+    internal class PolymorphicValueTests : PocoTestFixture<PolymorphicValue>
+    {
+        public PolymorphicValueTests()
+        {
+            AddSample(
+                new PolymorphicValue(P(1)),
+                "",
+                @"",
+                @"");
+
+            AddSample(
+                new PolymorphicValue(new EnumValue(FNF)),
+                "",
+                @"",
+                @"");
+
+            AddSample(
+                new PolymorphicValue(new StringValue("seven")),
+                "",
+                @"",
+                @"");
+
+            AddSample(
+                new PolymorphicValue(new NullableStringValue(null)),
+                "",
+                @"",
+                @"");
+
+            AddSample(
+                new PolymorphicValue(new ClassValue(P(1))),
+                "",
+                @"",
+                @"");
+
+            AddSample(
+                new PolymorphicValue(new NullableClassValue(null)),
+                "",
+                @"",
+                @"");
+
+            AddSample(
+                new PolymorphicValue(new PolymorphicValue(P(2))),
+                "",
+                @"",
+                @"");
+        }
+    }
+}
