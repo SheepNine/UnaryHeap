aaf0300dd811eb9b0a99d33fe62057d7f7be11b3
SheepNine <sheepnine@gmail.com>
2024-03-31 15:10:53 -0400

    Add hinting test

--- source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
+++ source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
@@ -67,5 +67,42 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             Assert.IsTrue(result.BackChild.IsLeaf);
             Assert.AreEqual(3, result.BackChild.SurfaceCount);
         }
+
+        [Test]
+        public void LShapeHinted()
+        {
+            var points = new Point2D[]
+            {
+                new(1, 1),
+                new(-1, 1),
+                new(-1, 0),
+                new(0, 0),
+                new(0, -1),
+                new(1, -1),
+            };
+            var sut = new Graph2D(true);
+            sut.AddVertex(points[0]);
+            sut.AddVertex(points[1]);
+            sut.AddVertex(points[2]);
+            sut.AddVertex(points[3]);
+            sut.AddVertex(points[4]);
+            sut.AddVertex(points[5]);
+            sut.AddEdge(points[0], points[1]);
+            sut.AddEdge(points[1], points[2]);
+            sut.AddEdge(points[2], points[3]);
+            sut.AddEdge(points[3], points[4]);
+            sut.AddEdge(points[4], points[5]);
+            sut.AddEdge(points[5], points[0]);
+            sut.AddEdge(points[0], points[3]);
+            sut.SetEdgeMetadatum(points[0], points[3], "hint", "0");
+
+            var result = sut.ConstructBspTree();
+
+            Assert.IsFalse(result.IsLeaf);
+            Assert.IsTrue(result.FrontChild.IsLeaf);
+            Assert.AreEqual(3, result.FrontChild.SurfaceCount);
+            Assert.IsTrue(result.BackChild.IsLeaf);
+            Assert.AreEqual(3, result.BackChild.SurfaceCount);
+        }
     }
 }
