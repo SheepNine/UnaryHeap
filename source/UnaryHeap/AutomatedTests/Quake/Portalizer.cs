f64b32cb8808fab0b86980ca813557dbdbaa6548
SheepNine <sheepnine@gmail.com>
2024-03-31 21:27:04 -0400

    Begin sketching out a portalizer

---
+++ source/UnaryHeap/AutomatedTests/Quake/Portalizer.cs
@@ -0,0 +1,87 @@
+﻿using Quake;
+using System;
+using System.Collections.Generic;
+using UnaryHeap.Algorithms;
+using UnaryHeap.DataType;
+
+namespace AutomatedTests.Quake
+{
+    abstract class Portalizer<TSurface, TPlane, TFacet, TBounds>
+    {
+        public void Portalize(IBspNode<TSurface, TPlane> root)
+        {
+            var bounds = CalculateBoundingBox(root);
+
+        }
+
+        private TBounds CalculateBoundingBox(IBspNode<TSurface, TPlane> root)
+        {
+            if (root.IsLeaf)
+            {
+                return UnionBounds(
+                    CalculateBoundingBox(root.FrontChild),
+                    CalculateBoundingBox(root.BackChild)
+                );
+            } else
+            {
+                return CalculateBounds(root.Surfaces);
+            }
+        }
+
+        protected abstract TBounds CalculateBounds(IEnumerable<TSurface> surfaces);
+        protected abstract TBounds UnionBounds(TBounds a, TBounds b);
+
+        class Portal
+        {
+            public TFacet Facet { get; private set; }
+            public IBspNode<TSurface, TFacet> Front { get; private set; }
+            public IBspNode<TSurface, TFacet> Back { get; private set; }
+
+            public Portal(TFacet facet, IBspNode<TSurface, TFacet> front,
+                IBspNode<TSurface, TFacet> back)
+            {
+                Facet = facet;
+                Front = front;
+                Back = back;
+            }
+
+            public void Split(Func<TFacet, Tuple<TFacet, TFacet>> splitFunc,
+                out Portal front, out Portal back)
+            {
+                var splitFacets = splitFunc(Facet);
+                if (splitFacets.Item1 == null)
+                    front = null;
+                else
+                    front = new Portal(splitFacets.Item1, Front, Back);
+
+                if (splitFacets.Item2 == null)
+                    back = null;
+                else
+                    back = new Portal(splitFacets.Item2, Front, Back);
+            }
+        }
+    }
+
+    class QuakePortalizer: Portalizer<Facet, Hyperplane3D, Facet3D, Orthotope3D>
+    {
+        protected override Orthotope3D CalculateBounds(IEnumerable<Facet> surfaces)
+        {
+            return null;
+        }
+
+        protected override Orthotope3D UnionBounds(Orthotope3D a, Orthotope3D b)
+        {
+            return null;
+        }
+    }
+
+    class Orthotope3D
+    {
+
+    }
+
+    class Facet3D
+    {
+
+    }
+}
