786a081977fa19d65db1bff792e595f469750ee5
SheepNine <sheepnine@gmail.com>
2024-04-03 21:39:03 -0400

    Add Graph2DBsp test case

--- source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
+++ source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
@@ -109,6 +109,8 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             // Single split should have one portal
             var portalSet = new GraphPortalizer().Portalize(tree).ToList();
             Assert.AreEqual(1, portalSet.Count);
+            // Expectations are brittle; are order and facing sensitive
+            // Expectations don't check leaves
             Assert.AreEqual(new Point2D(0, 0), portalSet[0].Facet.Start);
             Assert.AreEqual(new Point2D(1, 1), portalSet[0].Facet.End);
         }
@@ -152,6 +154,58 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             // Separated leaves should have no portals between them
             var portalSet = new GraphPortalizer().Portalize(tree).ToList();
             Assert.AreEqual(0, portalSet.Count);
+        }
+
+        [Test]
+        public void UShape()
+        {
+            var points = new Point2D[]
+            {
+                new(2, -2),
+                new(2, 1),
+                new(0, 2),
+                new(-2, 1),
+                new(-2, -2),
+
+                new(-1, -2),
+                new(-1, 1),
+                new(1, 1),
+                new(1, -2),
+            };
+
+            var sut = new Graph2D(true);
+            foreach (var point in points)
+                sut.AddVertex(point);
+            sut.AddEdge(points[0], points[1]);
+            sut.AddEdge(points[1], points[2]);
+            sut.AddEdge(points[2], points[3]);
+            sut.AddEdge(points[3], points[4]);
+            sut.AddEdge(points[4], points[5]);
+            sut.AddEdge(points[5], points[6]);
+            sut.AddEdge(points[6], points[7]);
+            sut.AddEdge(points[7], points[8]);
+            sut.AddEdge(points[8], points[0]);
+
+            var tree = sut.ConstructBspTree();
+
+            Assert.IsFalse(tree.IsLeaf);
+            Assert.IsTrue(tree.FrontChild.IsLeaf);
+            Assert.AreEqual(3, tree.FrontChild.SurfaceCount);
+            Assert.IsFalse(tree.BackChild.IsLeaf);
+            Assert.IsTrue(tree.BackChild.FrontChild.IsLeaf);
+            Assert.AreEqual(3, tree.FrontChild.SurfaceCount);
+            Assert.IsTrue(tree.BackChild.BackChild.IsLeaf);
+            Assert.AreEqual(3, tree.FrontChild.SurfaceCount);
+
+            // Two portals between center and edges
+            var portalSet = new GraphPortalizer().Portalize(tree).ToList();
+            Assert.AreEqual(2, portalSet.Count);
+            // Expectations are brittle; are order and facing sensitive
+            // Expectations don't check leaves
+            Assert.AreEqual(new Point2D(-1, 1), portalSet[0].Facet.Start);
+            Assert.AreEqual(new Point2D(-2, 1), portalSet[0].Facet.End);
+            Assert.AreEqual(new Point2D(2, 1), portalSet[1].Facet.Start);
+            Assert.AreEqual(new Point2D(1, 1), portalSet[1].Facet.End);
         }
     }
 }
