1614b5c9ee3bc4f811d8171c263e090839006c9b
SheepNine <sheepnine@gmail.com>
2024-04-05 16:10:40 -0400

    Pull portalizer into algorithm library

---
+++ source/UnaryHeap/UnaryHeap.GraphAlgorithms/Graph2DPortalizer.cs
@@ -0,0 +1,13 @@
+﻿using UnaryHeap.Algorithms;
+using UnaryHeap.DataType;
+using UnaryHeap.Graph;
+
+namespace UnaryHeap.GraphAlgorithms
+{
+    public class GraphPortalizer : Portalizer<GraphSegment, Hyperplane2D, Facet2D, Orthotope2D>
+    {
+        public GraphPortalizer() : base(GraphDimension.Instance)
+        {
+        }
+    }
+}
