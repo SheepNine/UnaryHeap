71c81368c13ff95cb399aaa17e7a52a413d004bb
SheepNine <sheepnine@gmail.com>
2024-04-16 11:10:30 -0400

    Rewrite Portalize() method to use Portalization class

--- source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
+++ source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
@@ -12,33 +12,6 @@ namespace UnaryHeap.GraphAlgorithms.Tests
     public class Graph2DBinarySpacePartitionerTests
     {
         [Test]
-        public void JustTwoFacetsNbd()
-        {
-            var points = new Point2D[]
-            {
-                new (1, -1),
-                new (-1, 0),
-                new (-1, 1),
-                new (-1, 2),
-            };
-            var graph = new Graph2D(true);
-            foreach (var point in points)
-                graph.AddVertex(point);
-            graph.AddEdge(points[0], points[1]);
-            graph.AddEdge(points[1], points[2]);
-            graph.AddEdge(points[2], points[3]);
-            graph.SetEdgeMetadatum(points[2], points[3], "hint", "0");
-
-
-            var tree = graph.ConstructBspTree();
-            Assert.AreEqual(3, tree.NodeCount);
-            Assert.AreEqual(new Hyperplane2D(-1, 0, -1), tree.PartitionPlane);
-
-            var portals = Portalize(tree).ToList();
-            Assert.AreEqual(0, portals.Count);
-        }
-
-        [Test]
         public void ConvexBox()
         {
             var points = new Point2D[]
@@ -59,7 +32,7 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             var tree = graph.ConstructBspTree();
             Assert.IsTrue(tree.IsLeaf);
             Assert.AreEqual(4, tree.SurfaceCount);
-            var portalSet = tree.Portalize().ToList();
+            var portalSet = tree.Portalize((s) => true).ToList();
             Assert.AreEqual(0, portalSet.Count);
         }
 
@@ -83,8 +56,8 @@ namespace UnaryHeap.GraphAlgorithms.Tests
 
             var bspTree = graph.ConstructBspTree();
             Assert.AreEqual(7, bspTree.NodeCount);
-            var portalSet = bspTree.Portalize().ToList();
-            Assert.AreEqual(0, portalSet.Count);
+            var portalSet = bspTree.Portalize((s) => true).ToList();
+            Assert.AreEqual(4, portalSet.Count);
         }
 
         [Test]
@@ -118,10 +91,10 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             Assert.AreEqual(3, tree.BackChild.SurfaceCount);
 
             // Single split should have one portal
-            var portalSet = tree.Portalize().ToList();
-            Assert.AreEqual(1, portalSet.Count);
-            Assert.AreEqual(new Point2D(2, 0), portalSet[0].Facet.Start);
-            Assert.AreEqual(new Point2D(0, 0), portalSet[0].Facet.End);
+            var portalSet = tree.Portalize((s) => true).ToList();
+            CheckPortals(tree, portalSet, @"
+                (F.) [0,0] -> [2,0] (B.)
+            ");
 
             // All leaves are interior so culling should not remove anything
             var culledTree = tree.CullOutside(portalSet,
@@ -153,10 +126,6 @@ namespace UnaryHeap.GraphAlgorithms.Tests
 
             var bspTree = graph.ConstructBspTree();
             Assert.AreEqual(9, bspTree.NodeCount);
-            var portalSet = bspTree.Portalize().ToList();
-            Assert.AreEqual(1, portalSet.Count);
-            Assert.AreEqual(new Point2D(-2, 0), portalSet[0].Facet.Start);
-            Assert.AreEqual(new Point2D(-1, 0), portalSet[0].Facet.End);
         }
 
         [Test]
@@ -192,12 +161,10 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             Assert.AreEqual(3, tree.BackChild.SurfaceCount);
 
             // Single split should have one portal
-            var portalSet = tree.Portalize().ToList();
-            Assert.AreEqual(1, portalSet.Count);
-            // Expectations are brittle; are order and facing sensitive
-            // Expectations don't check leaves
-            Assert.AreEqual(new Point2D(0, 0), portalSet[0].Facet.Start);
-            Assert.AreEqual(new Point2D(1, 1), portalSet[0].Facet.End);
+            var portalSet = tree.Portalize((s) => true).ToList();
+            CheckPortals(tree, portalSet, @"
+                (F.) [1,1] -> [0,0] (B.)
+            ");
         }
 
         [Test]
@@ -237,7 +204,7 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             Assert.AreEqual(4, tree.BackChild.SurfaceCount);
 
             // Separated leaves should have no portals between them
-            var portalSet = tree.Portalize().ToList();
+            var portalSet = tree.Portalize((s) => true).ToList();
             Assert.AreEqual(0, portalSet.Count);
 
             var culledTree = tree.CullOutside(portalSet, new[] { new Point2D(2, 2) });
@@ -286,14 +253,11 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             Assert.AreEqual(3, tree.FrontChild.SurfaceCount);
 
             // Two portals between center and edges
-            var portalSet = tree.Portalize().ToList();
-            Assert.AreEqual(2, portalSet.Count);
-            // Expectations are brittle; are order and facing sensitive
-            // Expectations don't check leaves
-            Assert.AreEqual(new Point2D(-1, 1), portalSet[0].Facet.Start);
-            Assert.AreEqual(new Point2D(-2, 1), portalSet[0].Facet.End);
-            Assert.AreEqual(new Point2D(2, 1), portalSet[1].Facet.Start);
-            Assert.AreEqual(new Point2D(1, 1), portalSet[1].Facet.End);
+            var portalSet = tree.Portalize((s) => true).ToList();
+            CheckPortals(tree, portalSet, @"
+                (F.) [-2,1] -> [-1,1] (BF.)
+                (F.) [1,1] -> [2,1] (BB.)
+            ");
         }
 
         [Test]
@@ -334,7 +298,7 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             var tree = sut.ConstructBspTree();
             Assert.AreEqual(9, tree.NodeCount);
 
-            var portalSet = tree.Portalize().ToList();
+            var portalSet = tree.Portalize((s) => true).ToList();
             Assert.AreEqual(4, portalSet.Count);
 
             var middleRoomTree = tree.CullOutside(portalSet, new[] { new Point2D(0, 0) });
@@ -392,9 +356,7 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             var tree = sut.ConstructBspTree();
             Assert.AreEqual(17, tree.NodeCount);
 
-            var portalSet = tree.Portalize().ToList();
-            Assert.AreEqual(0, portalSet.Count);
-
+            var portalSet = tree.Portalize((s) => true).ToList();
             var middleRoomTree = tree.CullOutside(portalSet, new[] { new Point2D(0, 0) });
             Assert.IsTrue(middleRoomTree.IsLeaf);
         }
@@ -418,16 +380,10 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             sut.AddEdge(points[0], points[1]);
             sut.AddEdge(points[1], points[2]);
             sut.AddEdge(points[2], points[3]);
-            sut.SetEdgeMetadatum(points[0], points[1], "frontsector", "1");
-            sut.SetEdgeMetadatum(points[1], points[2], "frontsector", "1");
-            sut.SetEdgeMetadatum(points[2], points[3], "frontsector", "1");
 
             sut.AddEdge(points[3], points[4]);
             sut.AddEdge(points[4], points[5]);
             sut.AddEdge(points[5], points[0]);
-            sut.SetEdgeMetadatum(points[3], points[4], "frontsector", "2");
-            sut.SetEdgeMetadatum(points[4], points[5], "frontsector", "2");
-            sut.SetEdgeMetadatum(points[5], points[0], "frontsector", "2");
 
             sut.AddEdge(points[3], points[0]);
             sut.SetEdgeMetadatum(points[3], points[0], "frontsector", "1");
@@ -438,7 +394,7 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             Assert.AreEqual(4, tree.FrontChild.SurfaceCount);
             Assert.AreEqual(4, tree.BackChild.SurfaceCount);
 
-            var portals = Portalize(tree).ToList();
+            var portals = Portalize(tree, s => s.BackMaterial == 3).ToList();
             // TODO: this ended up working first try, but is there a geometry configuration
             // which causes the two-sided surfaces to close portals?
             Assert.AreEqual(1, portals.Count);
@@ -767,6 +723,7 @@ namespace UnaryHeap.GraphAlgorithms.Tests
         [Test]
         public void ButteJoins()
         {
+            const int SOLID = 10;
             var points = new Point2D[]
             {
                 new(2, 1),
@@ -792,69 +749,67 @@ namespace UnaryHeap.GraphAlgorithms.Tests
 
             var brushes = new[]
             {
-                MakeBrush(0, 1, points[0], points[1], points[2], points[3]),
-                MakeBrush(1, 1, points[4], points[5], points[6], points[7]),
-                MakeBrush(2, 1, points[8], points[9], points[10], points[11]),
-                MakeBrush(3, 1, points[12], points[13], points[14], points[15]),
+                MakeBrush(0, SOLID, points[0], points[1], points[2], points[3]),
+                MakeBrush(1, SOLID, points[4], points[5], points[6], points[7]),
+                MakeBrush(2, SOLID, points[8], points[9], points[10], points[11]),
+                MakeBrush(3, SOLID, points[12], points[13], points[14], points[15]),
             };
 
             var surfaces = ConstructSolidGeometry(brushes);
             CheckCsgOutput(surfaces, @"
-                B2 (0) [2,2] -> [2,3] (1)
-                B2 (0) [2,3] -> [3,3] (1)
-                B2 (0) [3,3] -> [3,2] (1)
-                B2 (0) [3,2] -> [3,1] (1)
-                B2 (0) [3,1] -> [3,-1] (1)
-                B2 (0) [3,-1] -> [3,-2] (1)
-                B2 (0) [3,-2] -> [3,-3] (1)
-                B2 (0) [3,-3] -> [2,-3] (1)
-                B2 (0) [2,-3] -> [2,-2] (1)
-                B1 (0) [2,-2] -> [-2,-2] (1)
-                B3 (0) [-2,-2] -> [-2,-3] (1)
-                B3 (0) [-2,-3] -> [-3,-3] (1)
-                B3 (0) [-3,-3] -> [-3,-2] (1)
-                B3 (0) [-3,-2] -> [-3,1] (1)
-                B3 (0) [-3,1] -> [-3,3] (1)
-                B3 (0) [-3,3] -> [-2,3] (1)
-                B3 (0) [-2,3] -> [-2,2] (1)
-                B0 (0) [-2,2] -> [2,2] (1)
+                B2 (0) [2,2] -> [2,3] (10)
+                B2 (0) [2,3] -> [3,3] (10)
+                B2 (0) [3,3] -> [3,2] (10)
+                B2 (0) [3,2] -> [3,1] (10)
+                B2 (0) [3,1] -> [3,-1] (10)
+                B2 (0) [3,-1] -> [3,-2] (10)
+                B2 (0) [3,-2] -> [3,-3] (10)
+                B2 (0) [3,-3] -> [2,-3] (10)
+                B2 (0) [2,-3] -> [2,-2] (10)
+                B1 (0) [2,-2] -> [-2,-2] (10)
+                B3 (0) [-2,-2] -> [-2,-3] (10)
+                B3 (0) [-2,-3] -> [-3,-3] (10)
+                B3 (0) [-3,-3] -> [-3,-2] (10)
+                B3 (0) [-3,-2] -> [-3,1] (10)
+                B3 (0) [-3,1] -> [-3,3] (10)
+                B3 (0) [-3,3] -> [-2,3] (10)
+                B3 (0) [-2,3] -> [-2,2] (10)
+                B0 (0) [-2,2] -> [2,2] (10)
 
-                B0 (0) [2,1] -> [-2,1] (1)
-                B3 (0) [-2,1] -> [-2,-1] (1)
-                B1 (0) [-2,-1] -> [2,-1] (1)
-                B2 (0) [2,-1] -> [2,1] (1)
+                B0 (0) [2,1] -> [-2,1] (10)
+                B3 (0) [-2,1] -> [-2,-1] (10)
+                B1 (0) [-2,-1] -> [2,-1] (10)
+                B2 (0) [2,-1] -> [2,1] (10)
 
-                B0 (1) [-2,1] -> [2,1] (0)
-                B3 (1) [-2,-1] -> [-2,1] (0)
-                B1 (1) [2,-1] -> [-2,-1] (0)
-                B2 (1) [2,1] -> [2,-1] (0)
+                B0 (10) [-2,1] -> [2,1] (0)
+                B3 (10) [-2,-1] -> [-2,1] (0)
+                B1 (10) [2,-1] -> [-2,-1] (0)
+                B2 (10) [2,1] -> [2,-1] (0)
 
-                B2 (1) [2,3] -> [2,2] (0)
-                B0 (1) [2,2] -> [-2,2] (0)
-                B3 (1) [-2,2] -> [-2,3] (0)
-                B3 (1) [-2,3] -> [-3,3] (0)
-                B3 (1) [-3,3] -> [-3,1] (0)
-                B3 (1) [-3,1] -> [-3,-2] (0)
-                B3 (1) [-3,-2] -> [-3,-3] (0)
-                B3 (1) [-3,-3] -> [-2,-3] (0)
-                B3 (1) [-2,-3] -> [-2,-2] (0)
-                B1 (1) [-2,-2] -> [2,-2] (0)
-                B2 (1) [2,-2] -> [2,-3] (0)
-                B2 (1) [2,-3] -> [3,-3] (0)
-                B2 (1) [3,-3] -> [3,-2] (0)
-                B2 (1) [3,-2] -> [3,-1] (0)
-                B2 (1) [3,-1] -> [3,1] (0)
-                B2 (1) [3,1] -> [3,2] (0)
-                B2 (1) [3,2] -> [3,3] (0)
-                B2 (1) [3,3] -> [2,3] (0)
+                B2 (10) [2,3] -> [2,2] (0)
+                B0 (10) [2,2] -> [-2,2] (0)
+                B3 (10) [-2,2] -> [-2,3] (0)
+                B3 (10) [-2,3] -> [-3,3] (0)
+                B3 (10) [-3,3] -> [-3,1] (0)
+                B3 (10) [-3,1] -> [-3,-2] (0)
+                B3 (10) [-3,-2] -> [-3,-3] (0)
+                B3 (10) [-3,-3] -> [-2,-3] (0)
+                B3 (10) [-2,-3] -> [-2,-2] (0)
+                B1 (10) [-2,-2] -> [2,-2] (0)
+                B2 (10) [2,-2] -> [2,-3] (0)
+                B2 (10) [2,-3] -> [3,-3] (0)
+                B2 (10) [3,-3] -> [3,-2] (0)
+                B2 (10) [3,-2] -> [3,-1] (0)
+                B2 (10) [3,-1] -> [3,1] (0)
+                B2 (10) [3,1] -> [3,2] (0)
+                B2 (10) [3,2] -> [3,3] (0)
+                B2 (10) [3,3] -> [2,3] (0)
             ");
 
             var tree = ConstructBspTree(
                 surfaces.Where(s => s.FrontMaterial != 1));
 
-            var portalSet = tree.Portalize().ToList();
-            Assert.AreEqual(0, portalSet.Count);
-
+            var portalSet = tree.Portalize((s) => s.BackMaterial == SOLID).ToList();
             var middleRoomTree = tree.CullOutside(portalSet, new[] { new Point2D(0, 0) });
             Assert.IsTrue(middleRoomTree.IsLeaf);
         }
@@ -882,19 +837,14 @@ namespace UnaryHeap.GraphAlgorithms.Tests
             var surfaces = ConstructSolidGeometry(brushes);
             var rawTree = ConstructBspTree(
                 surfaces.Where(s => s.FrontMaterial != SOLID));
-            var rawPortals = Portalize(rawTree).ToList();
-            CheckPortals(rawTree, rawPortals, @"
-                (FFFBBB.) [5,-9] -> [5,5] (FFFBBF.)
-                (FFFBBB.) [5,5] -> [-9,5] (FFFBF.)
-                (FFFBBF.) [9,5] -> [5,5] (FFFBF.)
-            ");
+            var rawPortals = Portalize(rawTree, (s) => s.BackMaterial == SOLID).ToList();
             var culledTree = CullOutside(
                 rawTree, rawPortals, interiorPoints);
-            var cullPortals = Portalize(culledTree).ToList();
+            var cullPortals = Portalize(culledTree, (s) => s.BackMaterial == SOLID).ToList();
             CheckPortals(culledTree, cullPortals, @"
-                (BF.) [9,5] -> [5,5] (F.)
-                (BB.) [5,5] -> [-9,5] (F.)
-                (BB.) [5,-9] -> [5,5] (BF.)
+                (F.) [5,5] -> [9,5] (BF.)
+                (F.) [-9,5] -> [5,5] (BB.)
+                (BF.) [5,5] -> [5,-9] (BB.)
             ");
         }
 
@@ -928,42 +878,15 @@ namespace UnaryHeap.GraphAlgorithms.Tests
 
             var rawTree = ConstructBspTree(
                 surfaces.Where(s => s.FrontMaterial != SOLID));
-            var rawPortals = Portalize(rawTree).ToList();
-            CheckPortals(rawTree, rawPortals, @"
-                (FFFFBF.) [4,14] -> [0,10] (FFFFF.)
-                (FFFFBB.) [0,10] -> [-14,-4] (FFFFF.)
-                (FFFFBB.) [14,-4] -> [0,10] (FFFFBF.)
-                // These ones should not be here?
-                (FBBF.) [-20,-2] -> [-19,-1] (FFB.)
-                (BBF.) [-1,-19] -> [-2,-20] (FFB.)
-                (FBBB.) [1,19] -> [2,20] (FFFB.)
-                (BBB.) [20,2] -> [19,1] (FFFB.)
-            ");
+            var rawPortals = Portalize(rawTree, (s) => s.BackMaterial == SOLID).ToList();
             var culledTree = CullOutside(
                 rawTree, rawPortals, interiorPoints);
-            var cullPortals = Portalize(culledTree).ToList();
+            var cullPortals = Portalize(culledTree, (s) => s.BackMaterial == SOLID).ToList();
             CheckPortals(culledTree, cullPortals, @"
-                (BF.) [4,14] -> [0,10] (F.)
-                (BB.) [0,10] -> [-14,-4] (F.)
-                (BB.) [14,-4] -> [0,10] (BF.)
+                (F.) [0,10] -> [4,14] (BF.)
+                (F.) [-14,-4] -> [0,10] (BB.)
+                (BF.) [0,10] -> [14,-4] (BB.)
             ");
-        }
-
-        [Test]
-        public void CShape()
-        {
-            const int SOLID = 1;
-            var brushes = new[]
-            {
-                MakeBrush(0, SOLID, new Point2D[] { new(1, 0), new(0, 1), new(3, 2) }),
-                MakeBrush(1, SOLID, new Point2D[] { new(0, 3), new(1, 4), new(3, 2) }),
-            };
-            var surfaces = ConstructSolidGeometry(brushes)
-                .Where(s => s.FrontMaterial != SOLID);
-            var tree = ConstructBspTree(surfaces);
-            Assert.AreEqual(9, tree.NodeCount);
-            var portals = Portalize(tree).ToList();
-            CheckPortals(tree, portals, @"");
         }
 
         #region Test DSL
@@ -992,9 +915,10 @@ namespace UnaryHeap.GraphAlgorithms.Tests
                 surfaces);
         }
 
-        static List<GraphSpatial.Portal> Portalize(GraphSpatial.BspNode tree)
+        static List<GraphSpatial.Portal> Portalize(GraphSpatial.BspNode tree,
+            Func<GraphSegment, bool> solidPredicate)
         {
-            return GraphSpatial.Instance.Portalize(tree).ToList();
+            return GraphSpatial.Instance.Portalize(tree, solidPredicate).ToList();
         }
 
         static void CheckPortals(GraphSpatial.BspNode tree,
