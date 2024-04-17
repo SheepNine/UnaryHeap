324697d8976ef0e54e89d15e5ba96032966e5f39
SheepNine <sheepnine@gmail.com>
2024-04-17 11:56:01 -0400

    Capture rough timings of various steps

---
+++ source/UnaryHeap/QTwols/Instrumentation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Qtwols
+{
+    class Instrumentation
+    {
+        readonly Stopwatch jobWatch, stepWatch;
+
+        public Instrumentation()
+        {
+            jobWatch = new Stopwatch();
+            jobWatch.Start();
+            stepWatch = new Stopwatch();
+            stepWatch.Start();
+        }
+
+        public void StepComplete(string step)
+        {
+            stepWatch.Stop();
+            Console.WriteLine($"{step} completed in {stepWatch.ElapsedMilliseconds} ms");
+            stepWatch.Reset();
+            stepWatch.Start();
+        }
+
+        public void JobComplete()
+        {
+            jobWatch.Stop();
+            stepWatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"Total job time was {jobWatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
--- source/UnaryHeap/QTwols/Program.cs
+++ source/UnaryHeap/QTwols/Program.cs
@@ -9,16 +9,22 @@ namespace Qtwols
     {
         public static void Main(string[] args)
         {
+            var instrumentation = new Instrumentation();
+
             const string LEVEL = "E1M1";
             var inputFile = $"C:\\Users\\marsh\\source\\repos\\UnaryHeap\\quakeMaps\\{LEVEL}.MAP";
             var unculledOutput = $"C:\\Users\\marsh\\Documents\\FirstGoLang\\{LEVEL}_nocull.raw";
             var culledOutput = $"C:\\Users\\marsh\\Documents\\FirstGoLang\\{LEVEL}.raw";
 
             var entities = MapFileFormat.Load(inputFile);
+            instrumentation.StepComplete("Map loaded");
+
             var worldSpawnEntity = entities.Single(
                 entity => entity.Attributes["classname"] == "worldspawn");
             var brushes = worldSpawnEntity.Brushes
                 .Select(QuakeExtensions.CreateSpatialBrush).ToList();
+            instrumentation.StepComplete("Brushes calculated");
+
             var interiorPoints = entities.Where(e => e.NumBrushes == 0
                     && e.Attributes.ContainsKey("origin"))
                 .Select(e =>
@@ -30,20 +36,29 @@ namespace Qtwols
                         int.Parse(tokens[2], CultureInfo.InvariantCulture)
                     );
                 }).ToList();
+            // no instrumentation; finding interior points takes ~0ms
 
             var csgSurfaces = QuakeSpatial.Instance.ConstructSolidGeometry(brushes).Where(s =>
                 s.FrontMaterial != QuakeSpatial.SKY && s.FrontMaterial != QuakeSpatial.SOLID
             );
+            instrumentation.StepComplete("CSG computed");
+
             var unculledTree = QuakeSpatial.Instance.ConstructBspTree(
                 QuakeSpatial.Instance.ExhaustivePartitionStrategy(1, 10), csgSurfaces);
+            instrumentation.StepComplete("BSP computed");
+
             var portals = QuakeSpatial.Instance.Portalize(unculledTree, s =>
                 s.BackMaterial == QuakeSpatial.SKY || s.BackMaterial == QuakeSpatial.SOLID
             );
+            instrumentation.StepComplete("Portals computed");
+
             var culledTree = QuakeSpatial.Instance.CullOutside(
                 unculledTree, portals, interiorPoints);
+            instrumentation.StepComplete("Culled BSP computed");
 
             unculledTree.SaveRawFile(unculledOutput);
             culledTree.SaveRawFile(culledOutput);
+            instrumentation.JobComplete();
         }
     }
 }
