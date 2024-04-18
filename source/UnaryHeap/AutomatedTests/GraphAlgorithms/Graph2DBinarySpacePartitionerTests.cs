9b87cb8f1e7aabd9f369895a3852b2760ce4588c
SheepNine <sheepnine@gmail.com>
2024-04-17 21:52:06 -0400

    Add another test case

--- source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
+++ source/UnaryHeap/AutomatedTests/GraphAlgorithms/Graph2DBinarySpacePartitionerTests.cs
@@ -113,6 +113,30 @@ namespace UnaryHeap.GraphAlgorithms.Tests
         }
 
         [Test]
+        public void SBlock()
+        {
+            var builder = new GraphBuilder().WithPoints(
+                1, 0,
+                2, 0,
+                2, 2,
+                1, 2,
+                1, 3,
+                0, 3,
+                0, 1,
+                1, 1
+            ).WithPolygon(
+                0, 1, 2, 3, 4, 5, 6, 7
+            );
+
+            var tree = builder.ConstructBspTree();
+
+            var portalSet = Portalize(tree, s => true);
+            CheckPortals(tree, portalSet, @"
+                (F.) [1,1] -> [1,2] (B.)
+            ");
+        }
+
+        [Test]
         public void LShapeInverted()
         {
             var builder = new GraphBuilder().WithPoints(
