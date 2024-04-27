c195fa77a9e0e2321c64eba5b4b5bae1f034275c
SheepNine <sheepnine@gmail.com>
2024-04-27 11:29:54 -0400

    Clean up Quake tests; relocate spatial tests

--- source/UnaryHeap/AutomatedTests/Quake/QuakeSpatialTests.cs
+++ source/UnaryHeap/AutomatedTests/Quake/QuakeSpatialTests.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,69 +12,6 @@ namespace Quake
     public class QuakeSpatialTests
     {
         const string Dir = @"..\..\..\..\..\quakemaps";
-        const string RawOut = @"C:\Users\marsh\Documents\FirstGoLang";
-
-        [Test]
-        [Ignore("Use Qtwols for this now")]
-        public void DM7()
-        {
-            //X: -608:1520   Y: -432:3072   Z: -608:288
-
-            if (!Directory.Exists(Dir))
-                throw new InconclusiveException("No maps to test");
-
-            var entities = MapFileFormat.Load(Path.Combine(Dir, "E1M1.MAP"));
-            var worldSpawn = entities.Single(
-                entity => entity.Attributes["classname"] == "worldspawn");
-            var brushes = worldSpawn.Brushes
-                .Select(QuakeExtensions.CreateSpatialBrush).ToList();
-            var surfaces = QuakeSpatial.Instance.ConstructSolidGeometry(brushes)
-                .Where(s => s.FrontMaterial != QuakeSpatial.SOLID);
-            var rawTree = QuakeSpatial.Instance.ConstructBspTree(
-                QuakeSpatial.Instance.AxialPartitionStrategy(), surfaces);
-            Console.WriteLine(QuakeSpatial.Instance.CalculateBoundingBox(rawTree));
-            QuakeSpatial.Instance.Portalize(rawTree,
-                out IEnumerable<QuakeSpatial.Portal> portals,
-                out _);
-            var interiorPoints = entities.Where(e => e.NumBrushes == 0
-                    && e.Attributes.ContainsKey("origin"))
-                .Select(e =>
-                {
-                    var tokens = e.Attributes["origin"].Split();
-                    return new Point3D(
-                        int.Parse(tokens[0]),
-                        int.Parse(tokens[1]),
-                        int.Parse(tokens[2])
-                    );
-                }).ToList();
-            var culledTree = QuakeSpatial.Instance.CullOutside(rawTree, portals, interiorPoints);
-
-            if (Directory.Exists(RawOut))
-            {
-                rawTree.SaveRawFile(Path.Combine(RawOut, "e1m1_nocull.raw"));
-                culledTree.SaveRawFile(Path.Combine(RawOut, "e1m1.raw"));
-            }
-        }
-
-        [Test]
-        public void Rockets()
-        {
-            if (!Directory.Exists(Dir))
-                throw new InconclusiveException("No maps to test");
-
-            var entities = MapFileFormat.Load(Path.Combine(Dir, "B_ROCK0.MAP"));
-            var worldSpawn = entities.Single(
-                entity => entity.Attributes["classname"] == "worldspawn");
-            var brushes = worldSpawn.Brushes
-                .Select(QuakeExtensions.CreateSpatialBrush).ToList();
-            var surfaces = QuakeSpatial.Instance.ConstructSolidGeometry(brushes)
-                .Where(s => s.FrontMaterial != QuakeSpatial.SOLID);
-            var rawTree = QuakeSpatial.Instance.ConstructBspTree(
-                QuakeSpatial.Instance.ExhaustivePartitionStrategy(1, 10), surfaces);
-
-            if (Directory.Exists(RawOut))
-                rawTree.SaveRawFile(Path.Combine(RawOut, "b_rock0.raw"));
-        }
 
         static QuakeSpatial.Brush AABB(int material, Rational minX, Rational minY, Rational minZ,
             Rational maxX, Rational maxY, Rational maxZ)
@@ -122,9 +58,6 @@ namespace Quake
             Assert.AreEqual(3, culledTree.NodeCount);
             Assert.AreEqual(6, culledTree.FrontChild.SurfaceCount);
             Assert.AreEqual(6, culledTree.BackChild.SurfaceCount);
-
-            if (Directory.Exists(RawOut))
-                culledTree.SaveRawFile(Path.Combine(RawOut, "intersectingBox.raw"));
         }
 
         [Test]
@@ -161,9 +94,6 @@ namespace Quake
             Assert.AreEqual(3, culledTree.NodeCount);
             Assert.AreEqual(6, culledTree.FrontChild.SurfaceCount);
             Assert.AreEqual(6, culledTree.BackChild.SurfaceCount);
-
-            if (Directory.Exists(RawOut))
-                culledTree.SaveRawFile(Path.Combine(RawOut, "edgeBox.raw"));
         }
 
         [Test]
@@ -248,85 +178,52 @@ namespace Quake
         }
 
         [Test]
-        [Ignore("Needs conversion to CSG")]
-        public void DM2()
-        {
-            /*
-                Root: (-1)x + (0)y + (0)z + (2208)
-                FrontChild: (0)x + (1)y + (0)z + (832)
-                BackChild: (0)x + (-1)y + (0)z + (-896)
-             */
-            if (!Directory.Exists(Dir))
-                throw new InconclusiveException("No maps to test");
-
-            /*var output = MapFileFormat.Load(Path.Combine(Dir, "DM2.MAP"));
-            Assert.AreEqual(271, output.Length);
-            var worldSpawn = output.Single(
-                entity => entity.Attributes["classname"] == "worldspawn");
-            var facets = worldSpawn.Brushes.SelectMany(brush => brush.Facetize(100000)).ToList();
-            Assert.AreEqual(7239, facets.Count);
-            var rootPlane = new Hyperplane3D(-1, 0, 0, 2208);
-            var frontChildPlane = new Hyperplane3D(0, 1, 0, 832);
-            var backChildPlane = new Hyperplane3D(0, -1, 0, -896);
-            facets = facets
-                .Append(new QuakeSurface(new Facet3D(rootPlane, 1),
-                    new PlaneTexture("HINT0", 0, 0, 0, 0, 0), QuakeBSP.AIR, QuakeBSP.SOLID))
-                .ToList();
-            var tree = QuakeBSP.Instance.ConstructBspTree(
-                QuakeBSP.Instance.ExhaustivePartitionStrategy(1, 10), facets);
-            Assert.AreEqual(8667, tree.NodeCount);
-            var portalSet = QuakeBSP.Instance.Portalize(tree).ToList();
-            Assert.AreEqual(18265, portalSet.Count);
-            var interiorPoints = output.Where(e => e.NumBrushes == 0
-                    && e.Attributes.ContainsKey("origin"))
-                .Select(e =>
-                {
-                    var tokens = e.Attributes["origin"].Split();
-                    return new Point3D(
-                        int.Parse(tokens[0]),
-                        int.Parse(tokens[1]),
-                        int.Parse(tokens[2])
-                    );
-                }).ToList();
-            var culledTree = QuakeBSP.Instance.CullOutside(tree, portalSet, interiorPoints);
-            // TODO: 8667 - 8623 = 44; kind of expected more nodes to be exterior. Is
-            // the portalization giving wrong answers?
-            Assert.AreEqual(8623, culledTree.NodeCount);*/
-        }
-
-        [Test]
-        public void All()
+        public void VerifyInputBrushes()
         {
             if (!Directory.Exists(Dir))
                 throw new InconclusiveException("No maps to test");
 
             foreach (var file in Directory.GetFiles(Dir, "*.MAP"))
-            {
-                Console.WriteLine($"---{Path.GetFileNameWithoutExtension(file)}---");
-                var entities = MapFileFormat.Load(file);
-                var worldSpawn = entities.Single(
-                    entity => entity.Attributes["classname"] == "worldspawn");
-                
-                foreach (var mapBrush in worldSpawn.Brushes)
-                {
-                    var spatialBrush = QuakeExtensions.CreateSpatialBrush(mapBrush);
-                    if (spatialBrush.Surfaces.Count() != mapBrush.Planes.Count)
-                    {
-                        Console.WriteLine("Degenerate brush!");
-                        foreach (var plane in mapBrush.Planes)
-                        {
-                            var hp = new Hyperplane3D(
-                                new Point3D(plane.P3X, plane.P3Y, plane.P3Z),
-                                new Point3D(plane.P2X, plane.P2Y, plane.P2Z),
-                                new Point3D(plane.P1X, plane.P1Y, plane.P1Z));
+                VerifyMapBrushes(file);
+        }
 
-                            Console.WriteLine(plane);
-                            Console.WriteLine($"\t{hp}");
-                        }
-                        Assert.Fail();
-                    }
+        private static void VerifyMapBrushes(string mapFileName)
+        {
+            var entities = MapFileFormat.Load(mapFileName);
+            var worldSpawn = entities.Single(
+                entity => entity.Attributes["classname"] == "worldspawn");
+
+            foreach (var mapBrush in worldSpawn.Brushes)
+            {
+                try
+                {
+                    VerifyBrush(mapBrush);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("File: " + Path.GetFileNameWithoutExtension(mapFileName));
+                    throw;
                 }
             }
+        }
+
+        private static void VerifyBrush(MapBrush mapBrush)
+        {
+            var spatialBrush = QuakeExtensions.CreateSpatialBrush(mapBrush);
+            if (spatialBrush.Surfaces.Count() == mapBrush.Planes.Count)
+                return;
+
+            foreach (var plane in mapBrush.Planes)
+            {
+                var hp = new Hyperplane3D(
+                    new Point3D(plane.P3X, plane.P3Y, plane.P3Z),
+                    new Point3D(plane.P2X, plane.P2Y, plane.P2Z),
+                    new Point3D(plane.P1X, plane.P1Y, plane.P1Z));
+
+                Console.WriteLine(plane);
+                Console.WriteLine($"\t{hp}");
+            }
+            Assert.Fail();
         }
     }
 }
