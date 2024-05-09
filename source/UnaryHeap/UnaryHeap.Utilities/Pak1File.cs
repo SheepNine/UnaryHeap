f95b22f3eb1fa54e48b5a5bb3de520a97a42f940
SheepNine <sheepnine@gmail.com>
2024-05-08 22:20:36 -0400

    Add utility class to read Quake PAK files

---
+++ source/UnaryHeap/UnaryHeap.Utilities/Pak1File.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnaryHeap.Utilities
+{
+    /// <summary>
+    /// Class representing an idTech2 PAK file.
+    /// </summary>
+    public class Pak1File
+    {
+        class PakEntry
+        {
+            public string Name { get; private set; }
+            public int FileOffset { get; private set; }
+            public int DataSize { get; private set; }
+
+            public PakEntry(string name, int offset, int size)
+            {
+                Name = name;
+                FileOffset = offset;
+                DataSize = size;
+            }
+        }
+
+        readonly Stream datuh;
+        readonly List<PakEntry> manifest = new();
+
+        /// <summary>
+        /// Creates a new instance of the Pak1File class.
+        /// </summary>
+        /// <param name="path">The filename of the .PAK file to open.</param>
+        public Pak1File(string path)
+            : this(File.OpenRead(path))
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the Pak1File class.
+        /// </summary>
+        /// <param name="data">Stream containing the .PAK file data.</param>
+        public Pak1File(Stream data)
+        {
+            datuh = data;
+            datuh.Seek(0, SeekOrigin.Begin);
+            var magic = ReadString(4);
+            if (!magic.Equals("PACK", StringComparison.OrdinalIgnoreCase))
+                throw new InvalidDataException("Not a PACK file");
+            var contentsOffset = ReadLeInt32();
+            var contentCount = ReadLeInt32() / 64;
+            datuh.Seek(contentsOffset, SeekOrigin.Begin);
+            foreach (var i in Enumerable.Range(0, contentCount))
+            {
+                var name = ReadString(56);
+                var offset = ReadLeInt32();
+                var size = ReadLeInt32();
+                manifest.Add(new PakEntry(name, offset, size));
+            }
+        }
+
+        private int ReadLeInt32()
+        {
+            var buffer = ReadBytes(4);
+            return BitConverter.ToInt32(buffer, 0);
+        }
+
+        private string ReadString(int size)
+        {
+            return Encoding.ASCII.GetString(ReadBytes(size)).TrimEnd('\0');
+        }
+
+        private byte[] ReadBytes(int size)
+        {
+            var buffer = new byte[size];
+            var bytesRead = datuh.Read(buffer, 0, size);
+            if (bytesRead != size)
+                throw new InvalidDataException("Failed to read string");
+            return buffer;
+        }
+
+        /// <summary>
+        /// Gets the list of files in the PAK file.
+        /// </summary>
+        public IEnumerable<string> FileNames
+        {
+            get { return manifest.Select(m => m.Name); }
+        }
+    }
+}
