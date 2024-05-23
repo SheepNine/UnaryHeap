eddb1fba13258afc79075ec24637e27b3211e2bb
SheepNine <sheepnine@gmail.com>
2024-05-22 21:18:06 -0400

    Add tests for Point3DComparer class

--- source/UnaryHeap/AutomatedTests/DataType/Point3DComparerTests.cs
+++ source/UnaryHeap/AutomatedTests/DataType/Point3DComparerTests.cs
@@ -1,7 +1,32 @@
-﻿namespace UnaryHeap.DataType.Tests
+﻿using NUnit.Framework;
+
+namespace UnaryHeap.DataType.Tests
 {
+    [TestFixture]
     public class Point3DComparerTests
     {
-        // TODO: Write this
+        [Test]
+        public void ComparePoints()
+        {
+            var sortedPoints = new[]
+            {
+                null,
+                new Point3D(0, 0, 0),
+                new Point3D(0, 0, 1),
+                new Point3D(0, 1, 0),
+                new Point3D(1, 0, 0)
+            };
+
+            for (var i = 0; i < sortedPoints.Length; i++)
+            {
+                Assert.AreEqual(0, Point3DComparer.Instance.Compare(sortedPoints[i], sortedPoints[i]));
+
+                for (var j = i + 1; j < sortedPoints.Length; j++)
+                {
+                    Assert.AreEqual(-1, Point3DComparer.Instance.Compare(sortedPoints[i], sortedPoints[j]));
+                    Assert.AreEqual(1, Point3DComparer.Instance.Compare(sortedPoints[j], sortedPoints[i]));
+                }
+            }
+        }
     }
 }
