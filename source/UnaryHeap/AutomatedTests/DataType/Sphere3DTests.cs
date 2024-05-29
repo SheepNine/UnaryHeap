1f73dffd0598184a9c7ff6aee95618ab13e5c451
SheepNine <sheepnine@gmail.com>
2024-05-29 11:35:24 -0400

    Chip damage on Sphere3D test coverage

--- source/UnaryHeap/AutomatedTests/DataType/Sphere3DTests.cs
+++ source/UnaryHeap/AutomatedTests/DataType/Sphere3DTests.cs
@@ -1,7 +1,27 @@
-﻿namespace UnaryHeap.DataType.Tests
+﻿using NUnit.Framework;
+using System;
+
+namespace UnaryHeap.DataType.Tests
 {
     public class Sphere3DTests
     {
         // TODO: Write some tests
+
+
+        [Test]
+        public void SimpleArgumentExceptions()
+        {
+            var point = new Point3D(1, 2, 3);
+            TestUtils.NullChecks(new()
+            {
+                { typeof(ArgumentNullException), new TestDelegate[] {
+                    () => { _ = new Sphere3D(null, 1); },
+                    () => { _ = new Sphere3D(Point3D.Origin, null); },
+                    () => { Sphere3D.Circumcircle(null, point, point); },
+                    () => { Sphere3D.Circumcircle(point, null, point); },
+                    () => { Sphere3D.Circumcircle(point, point, null); },
+                }}
+            });
+        }
     }
 }
